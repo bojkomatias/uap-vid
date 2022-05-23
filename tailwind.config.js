2d17a782b1e36ee90b1c452508d70f8de4bb53d7
bojkomatias <bojko.matias@gmail.com>
2022-05-23 12:09:14 -0300

    Changes to metadata

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,4 +8,8 @@ module.exports = {
         './pages/**/*.{js,ts,jsx,tsx}',
         './components/**/*.{js,ts,jsx,tsx}',
     ],
+    plugins: [
+        require('@tailwindcss/typography'),
+        // ...
+    ],
 }
