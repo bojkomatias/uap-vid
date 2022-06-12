6dc175d0bdfb470bbfeb21bf9ec3b00ef2e037c1
amilcarrey <amilcarrey.ar@gmail.com>
2022-06-12 08:01:38 -0300

    Sign in with credentials with jwt

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,14 @@
 import NextAuth from 'next-auth'
 import AzureADB2CProvider from 'next-auth/providers/azure-ad-b2c'
+import getCollections, { CollectionName } from '../../../utils/bd/getCollection'
+
+import CredentialsProvider from 'next-auth/providers/credentials'
+
+import { compare } from 'bcryptjs'
 export default NextAuth({
-    // Configure one or more authentication providers
+    session: {
+        jwt: true,
+    },
     providers: [
         AzureADB2CProvider({
             tenantId: process.env.AZURE_AD_B2C_TENANT_NAME,
@@ -10,6 +17,42 @@ export default NextAuth({
             primaryUserFlow: process.env.AZURE_AD_B2C_PRIMARY_USER_FLOW,
             authorization: { params: { scope: 'offline_access openid' } },
         }),
-        // ...add more providers here
+        CredentialsProvider({
+            name: 'Credentials',
+            credentials: {
+                username: {
+                    label: 'Usuario',
+                    type: 'text',
+                    placeholder: 'jsmith@uap.edu.ar',
+                },
+                password: { label: 'Contraseña', type: 'password' },
+            },
+            async authorize(credentials) {
+                const users = getCollections(CollectionName.Users)
+                //Find user with the email
+                const result = await users.findOne({
+                    email: credentials.email,
+                })
+
+                //NextAuth maneja el error
+                if (!result) {
+                    client.close()
+                    throw new Error('No user found with the email')
+                }
+
+                //Check hased password with DB password
+                const checkPassword = await compare(
+                    credentials.passowrd,
+                    result.passowrd
+                )
+
+                if (!checkPassword) {
+                    client.close()
+                    throw new Error('Password doesnt match')
+                }
+
+                return { email: result.email }
+            },
+        }),
     ],
 })
