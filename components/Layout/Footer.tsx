e783893a3e2624e5154e517e5249cbb251c5ce0d
Nicolás Horn <nicoskate000@gmail.com>
2022-06-14 12:34:28 -0300

    changes to footer

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -2,8 +2,24 @@
 
 export default function Footer() {
     return (
-        <footer className="my-auto flex h-[10vh] items-center justify-center bg-primary text-xs text-white">
-            © COPYRIGHT 2021 - UNIVERSIDAD ADVENTISTA DEL PLATA
+        <footer className=" my-auto flex h-[10vh] flex-col items-center justify-center bg-primary text-center text-xs text-white">
+            <div className="flex gap-3">
+                <div>© COPYRIGHT 2022</div>
+                <a href="https://uap.edu.ar/">
+                    <p className="transition-all duration-200 hover:scale-105 hover:font-bold">
+                        {' '}
+                        UNIVERSIDAD ADVENTISTA DEL PLATA
+                    </p>
+                </a>{' '}
+            </div>
+            <div className="mt-2 flex gap-1">
+                <div>WebApp desarrollada por </div>
+                <a href="/">
+                    <p className="transition-all duration-200 hover:ml-1 hover:rotate-[35deg] hover:scale-125 hover:font-bold">
+                        TPC
+                    </p>
+                </a>
+            </div>
         </footer>
     )
 }
