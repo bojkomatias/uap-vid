80f3c3b33f183a6f82b40c929a4add84743ea195
Nicolás Horn <nicoskate000@gmail.com>
2022-06-15 01:05:44 -0300

    userlist page styling

--- pages/protected/userlist.tsx
+++ pages/protected/userlist.tsx
@@ -25,89 +25,117 @@ function UserList({ users }: any) {
     return (
         <div>
             {users.map((user: any) => (
-                <div key={user.email}>
-                    <pre>{JSON.stringify(user, null, 2)}</pre>
-                    <Listbox
-                        value={user.role}
-                        onChange={(e) => {
-                            user.role = e
-                            UpdateRoleForUser(user._id, e)
-                        }}
-                    >
-                        {({ open }) => (
-                            <>
-                                <div className="relative mt-1">
-                                    <Listbox.Button className="border-gray-300 focus:ring-indigo-500 focus:border-indigo-500 relative w-full cursor-default rounded-md border bg-white py-2 pl-3 pr-10 text-left shadow-sm focus:outline-none focus:ring-1 sm:text-sm">
-                                        <span className="block truncate">
-                                            {user.role}
-                                        </span>
-                                        <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-                                            <SelectorIcon
-                                                className="text-gray-400 h-5 w-5"
-                                                aria-hidden="true"
-                                            />
-                                        </span>
-                                    </Listbox.Button>
+                <div key={user.email} className="p-10 text-primary">
+                    <div className="flex items-center justify-around gap-6 text-xl font-bold">
+                        <p>Email</p>
+                        <p className="-translate-x-8">
+                            Último inicio de sesión
+                        </p>
+                        <p className="-translate-x-10">Rol</p>
+                    </div>
+                    <div className="flex items-center justify-around gap-6">
+                        <p>{user.email}</p>
+                        <div className="flex gap-3">
+                            {' '}
+                            <p>
+                                {new Date(user.lastLogin).toLocaleDateString(
+                                    'es-ar'
+                                )}
+                            </p>
+                            <p>
+                                {new Date(user.lastLogin).toLocaleTimeString(
+                                    'es-ar'
+                                )}
+                            </p>
+                        </div>
 
-                                    <Transition
-                                        show={open}
-                                        as={Fragment}
-                                        leave="transition ease-in duration-100"
-                                        leaveFrom="opacity-100"
-                                        leaveTo="opacity-0"
-                                    >
-                                        <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                                            {roles.map((role) => (
-                                                <Listbox.Option
-                                                    key={role}
-                                                    className={({ active }) =>
-                                                        classNames(
-                                                            active
-                                                                ? 'bg-indigo-600 text-white'
-                                                                : 'text-gray-900',
-                                                            'relative cursor-default select-none py-2 pl-3 pr-9'
-                                                        )
-                                                    }
-                                                    value={role}
-                                                >
-                                                    {({ selected, active }) => (
-                                                        <>
-                                                            <span
-                                                                className={classNames(
-                                                                    selected
-                                                                        ? 'font-semibold'
-                                                                        : 'font-normal',
-                                                                    'block truncate'
-                                                                )}
-                                                            >
-                                                                {role}
-                                                            </span>
+                        <Listbox
+                            value={user.role}
+                            onChange={(e) => {
+                                user.role = e
+                                UpdateRoleForUser(user._id, e)
+                            }}
+                        >
+                            {({ open }) => (
+                                <>
+                                    <div className="relative mt-1">
+                                        <Listbox.Button className="input w-40">
+                                            <span className="block truncate">
+                                                {user.role}
+                                            </span>
+                                            <span className="absolute inset-y-0 right-0 flex cursor-pointer items-center pr-2">
+                                                <SelectorIcon
+                                                    className="text-gray-400 h-5 w-5"
+                                                    aria-hidden="true"
+                                                />
+                                            </span>
+                                        </Listbox.Button>
 
-                                                            {selected ? (
+                                        <Transition
+                                            show={open}
+                                            as={Fragment}
+                                            leave="transition ease-in duration-100"
+                                            leaveFrom="opacity-100"
+                                            leaveTo="opacity-0"
+                                        >
+                                            <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 hover:text-base-600 focus:outline-none sm:text-sm">
+                                                {roles.map((role) => (
+                                                    <Listbox.Option
+                                                        key={role}
+                                                        className={({
+                                                            active,
+                                                        }) =>
+                                                            classNames(
+                                                                active
+                                                                    ? 'font-bold text-primary'
+                                                                    : 'hover:font-bold',
+                                                                'relative cursor-pointer select-none py-2 pl-3 pr-9 '
+                                                            )
+                                                        }
+                                                        value={role}
+                                                    >
+                                                        {({
+                                                            selected,
+                                                            active,
+                                                        }) => (
+                                                            <>
                                                                 <span
                                                                     className={classNames(
-                                                                        active
-                                                                            ? 'text-white'
-                                                                            : 'text-indigo-600',
-                                                                        'absolute inset-y-0 right-0 flex items-center pr-4'
+                                                                        selected
+                                                                            ? 'font-semibold'
+                                                                            : 'font-normal',
+                                                                        'block truncate'
                                                                     )}
                                                                 >
-                                                                    <CheckIcon
-                                                                        className="h-5 w-5"
-                                                                        aria-hidden="true"
-                                                                    />
+                                                                    {role}
                                                                 </span>
-                                                            ) : null}
-                                                        </>
-                                                    )}
-                                                </Listbox.Option>
-                                            ))}
-                                        </Listbox.Options>
-                                    </Transition>
-                                </div>
-                            </>
-                        )}
-                    </Listbox>
+
+                                                                {selected ? (
+                                                                    <span
+                                                                        className={classNames(
+                                                                            active
+                                                                                ? 'text-primary'
+                                                                                : 'text-indigo-600',
+                                                                            'absolute inset-y-0 right-0 flex items-center pr-4'
+                                                                        )}
+                                                                    >
+                                                                        <CheckIcon
+                                                                            className="h-5 w-5"
+                                                                            aria-hidden="true"
+                                                                        />
+                                                                    </span>
+                                                                ) : null}
+                                                            </>
+                                                        )}
+                                                    </Listbox.Option>
+                                                ))}
+                                            </Listbox.Options>
+                                        </Transition>
+                                    </div>
+                                </>
+                            )}
+                        </Listbox>
+                    </div>
                 </div>
             ))}
         </div>
