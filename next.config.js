d90ce19cf728508799d2012ad97560deb32dd578
bojkomatias <bojko.matias@gmail.com>
2022-07-10 18:46:35 -0300

    Typescript Vercel

--- next.config.js
+++ next.config.js
@@ -4,4 +4,11 @@ module.exports = {
     experimental: {
         outputStandalone: true,
     },
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+    },
 }
