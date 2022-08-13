c174c0cbc3aafa307c2ab5cae548ad9b1b68ff91
amilcarrey <amilcarrey.ar@gmail.com>
2022-08-13 02:47:08 -0300

    Fix build error with next/link

--- components/Layout/UserAuth.tsx
+++ components/Layout/UserAuth.tsx
@@ -1,5 +1,5 @@
 import { useSession, signIn, signOut } from 'next-auth/react'
-
+import Link from 'next/link'
 export default function UserAuth() {
     const { data: session } = useSession()
     if (session) {
@@ -7,7 +7,7 @@ export default function UserAuth() {
             <>
                 Bienvenido <b>{session?.user?.email} </b>{' '}
                 <div className="transition-all duration-150 hover:scale-[1.07]">
-                    <a href="/protected/profile">
+                    <Link href="/protected/profile" passHref >
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             className="h-8 w-8"
@@ -22,7 +22,7 @@ export default function UserAuth() {
                                 d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
                             />
                         </svg>
-                    </a>
+                    </Link>
                 </div>
             </>
         )
