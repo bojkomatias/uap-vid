22175bfea89dcdef996a1f163ad50847a8d66eaf
Nicolás Horn <nicoskate000@gmail.com>
2022-11-07 15:24:33 -0300

    Made textarea autoresizable based on content

--- components/Atomic/Textarea.tsx
+++ components/Atomic/Textarea.tsx
@@ -3,6 +3,7 @@ import { useProtocolContext } from '../../config/createContext'
 
 function handleKeyDown(e: any) {
     //Para hacer autoresize del textarea
+
     e.target.style.height = 'inherit'
     e.target.style.height = `${e.target.scrollHeight}px`
 }
@@ -15,19 +16,21 @@ const Textarea = ({
     const form = useProtocolContext()
 
     return (
-        <div className="m-3 p-1">
-            <label
-                className={`text-xs font-normal  uppercase 
+        <div>
+            <div className="m-3 p-1">
+                <label
+                    className={`text-xs font-normal  uppercase 
                             text-base-700/60`}
-            >
-                {label}
-            </label>
-            <textarea
-                {...form.getInputProps(path + x)}
-                onKeyDown={handleKeyDown}
-                className="input"
-                placeholder={label}
-            />
+                >
+                    {label}
+                </label>
+                <textarea
+                    {...form.getInputProps(path + x)}
+                    onKeyDown={handleKeyDown}
+                    className="input h-auto"
+                    placeholder={label}
+                />
+            </div>
         </div>
     )
 }
