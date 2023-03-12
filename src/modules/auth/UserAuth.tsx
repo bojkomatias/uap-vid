99941f6efbf32f9679a920ee64d6dafde0644a63
bojkomatias <bojko.matias@gmail.com>
2023-03-12 00:40:34 -0300

    Added clsx, UserAuth with a Headless Component

--- src/modules/auth/UserAuth.tsx
+++ src/modules/auth/UserAuth.tsx
@@ -1,62 +1,69 @@
 'use client'
-import { useSession, signIn, signOut } from 'next-auth/react'
+import { useSession, signOut } from 'next-auth/react'
 import Link from 'next/link'
-import { useEffect, useState } from 'react'
-import gsap from 'gsap'
+import { Fragment } from 'react'
+import { Menu, Transition } from '@headlessui/react'
+import { UserCircle } from 'tabler-icons-react'
+import clsx from 'clsx'
 
 export const UserAuth = () => {
     const { data: session } = useSession()
-
-    const [showCombo, setShowCombo] = useState(false)
-
-    useEffect(() => {
-        gsap.fromTo('#combo', { opacity: 0 }, { opacity: 1, duration: 0.3 })
-    }, [showCombo])
 
     if (session) {
         return (
             <>
                 Bienvenido <b>{session?.user?.email} </b>{' '}
-                <div
-                    className="relative cursor-pointer transition-all duration-150 "
-                    onClick={() => setShowCombo(!showCombo)}
-                >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-8 w-8 hover:scale-[1.07]"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        strokeWidth={2.1}
+                <Menu as="div" className="relative ml-1">
+                    <div>
+                        <Menu.Button className="group flex max-w-xs items-center rounded-full text-sm focus:outline-none focus:ring-1 focus:ring-white focus:ring-offset-primary">
+                            <span className="sr-only">Open user menu</span>
+                            <UserCircle className="h-8 w-8 stroke-[1.5px]" />
+                        </Menu.Button>
+                    </div>
+                    <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-100"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
                     >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-                        />
-                    </svg>
-
-                    {showCombo && (
-                        <div
-                            id="combo"
-                            className="absolute right-0 mt-2 w-40 bg-white p-4 text-center opacity-0 shadow-md"
-                        >
-                            <Link href="/protected/profile" passHref>
-                                <button className="mb-1 w-full p-2 text-sm font-bold text-primary transition-all duration-200 hover:bg-primary hover:text-white">
-                                    Perfil
-                                </button>
-                            </Link>
-                            <button
-                                className="w-full p-2 text-sm font-bold text-primary transition-all duration-200 hover:bg-primary hover:text-white"
-                                onClick={() => signOut({ callbackUrl: '/' })}
-                            >
-                                Cerrar sesión
-                            </button>
-                        </div>
-                    )}
-                </div>
+                        <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none space-y-1">
+                            <Menu.Item>
+                                {({ active }) => (
+                                    <Link
+                                        href="protected/profile"
+                                        className={clsx(
+                                            active ? 'bg-gray-100 ' : '',
+                                            'block px-6 py-2 text-sm text-gray-700 w-full text-left font-medium'
+                                        )}
+                                    >
+                                        Perfil
+                                    </Link>
+                                )}
+                            </Menu.Item>
+                            <Menu.Item>
+                                {({ active }) => (
+                                    <button
+                                        className={clsx(
+                                            active ? 'bg-gray-100 ' : '',
+                                            'block px-6 py-2 text-sm text-gray-700 w-full text-left font-medium'
+                                        )}
+                                        onClick={() =>
+                                            signOut({ callbackUrl: '/' })
+                                        }
+                                    >
+                                        Cerrar sesión
+                                    </button>
+                                )}
+                            </Menu.Item>
+                        </Menu.Items>
+                    </Transition>
+                </Menu>
             </>
         )
     }
+
     return <></>
 }
