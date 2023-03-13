b89a6b322b829bf338b135b110c246e37a53112d
bojkomatias <bojko.matias@gmail.com>
2023-03-13 13:43:35 -0300

    authOptions import on layout

--- src/app/protected/layout.tsx
+++ src/app/protected/layout.tsx
@@ -1,14 +1,13 @@
-import Navigation from '@auth/Navigation'
 import { getServerSession } from 'next-auth'
 import { redirect } from 'next/navigation'
-import NextAuth from 'pages/api/auth/[...nextauth]'
+import { authOptions } from 'pages/api/auth/[...nextauth]'
 
 export default async function Layout({
     children,
 }: {
     children: React.ReactNode
 }) {
-    const session = await getServerSession(NextAuth)
+    const session = await getServerSession(authOptions)
     if (!session) redirect('/')
 
     return children
