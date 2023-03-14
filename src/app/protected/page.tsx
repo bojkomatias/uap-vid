7a5fa72d3290b78da06854e7333eeff08e58b2f3
Nicolás Horn <nicoskate000@gmail.com>
2023-03-14 13:00:56 -0300

    Finished table refactor with tailwindUI

--- src/app/protected/page.tsx
+++ src/app/protected/page.tsx
@@ -4,39 +4,21 @@ import ItemView from '@protocol/ItemView'
 import NewProtocolButton from '@protocol/NewProtocolButton'
 import Link from 'next/link'
 import { getAllProtocols } from 'repositories/protocol'
+import Table from '@protocol/Table'
 
 // SSR Server Component, so no need to fetch from api endpoint
 export default async function Page() {
     const protocols = await getAllProtocols()
     return (
         <Navigation>
-            <Heading title="Lista de proyectos de investigación" />
             {/* //! Conditionally render if role allows it */}
-            <NewProtocolButton />
-            <div className="mx-auto mb-20 flex max-w-7xl flex-col justify-center px-20 py-10">
-                {protocols &&
-                    protocols.map((protocol) => (
-                        <div key={protocol.id} className="mt-5">
-                            {protocol?.sections && (
-                                <ItemView
-                                    dateOfCreation={protocol.createdAt}
-                                    identification={
-                                        protocol.sections.identification
-                                    }
-                                    id={protocol.id}
-                                />
-                            )}
-                        </div>
-                    ))}
 
-                {!protocols && (
-                    <div className="mt-12 flex w-full flex-col items-center gap-12">
-                        <p className="text-center font-thin uppercase text-primary">
-                            No hay proyectos cargados ...
-                        </p>
-                        <Link href="/protected">Volver</Link>
-                    </div>
-                )}
+            <div className="mx-auto mb-20 flex flex-col justify-center">
+                <Table
+                    protocols={protocols}
+                    title="Listado de proyectos de investigación"
+                    description="Lista de todos los protocolos cargados en el sistema, haz click en 'ver' para más detalles."
+                ></Table>
             </div>
         </Navigation>
     )
