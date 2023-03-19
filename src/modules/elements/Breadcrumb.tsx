dcd639be82df5b0935357e097eb9b4e288a67782
bojkomatias <bojko.matias@gmail.com>
2023-03-19 20:54:53 -0300

    change styles authored: Amilcar Rey

--- src/modules/elements/Breadcrumb.tsx
+++ src/modules/elements/Breadcrumb.tsx
@@ -11,7 +11,7 @@ const newLinkMap: any = {
     protocol: 'Panel de edición del protocolo',
 }
 
-export const Breadcrumb = ({ className }: { className: string }) => {
+export const Breadcrumb = () => {
     const pathname = usePathname()
     const breadcrumbs = useMemo(
         function generateBreadcrumbs() {
@@ -35,19 +35,18 @@ export const Breadcrumb = ({ className }: { className: string }) => {
     )
 
     return (
-        <nav className="mb-6 flex" aria-label="Breadcrumb">
-            <ol role="list" className="flex rounded-md bg-white px-4 shadow">
+        <nav className="mb-6 flex mt-6" aria-label="Breadcrumb">
+            <ol role="list" className="flex rounded-md bg-white px-4">
                 {pathname === '/protected' ? null : (
-                    <li className="flex">
-                        <div className="flex h-[44px] items-center">
-                            <Link
-                                href="/protected"
-                                className="text-primary/70 hover:text-primary"
-                            >
-                                <Home className="h-5 w-5 flex-shrink-0" />
-                                <span className="sr-only">Home</span>
-                            </Link>
-                        </div>
+                    <li className="flex items-center">
+                        <Link
+                            href="/protected"
+                            className="text-gray-500 hover:text-primary"
+                            passHref
+                        >
+                            <Home className="h-5 w-5 flex-shrink-0" />
+                            <span className="sr-only">Home</span>
+                        </Link>
                     </li>
                 )}
 
@@ -55,17 +54,10 @@ export const Breadcrumb = ({ className }: { className: string }) => {
                     <li key={page.name} className="flex">
                         {page.name == 'Inicio' ? null : (
                             <div className="flex items-center">
-                                <svg
-                                    className="h-full w-6 flex-shrink-0 text-gray-300"
-                                    viewBox="0 0 24 44"
-                                    preserveAspectRatio="none"
-                                    fill="currentColor"
-                                >
-                                    <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
-                                </svg>
+                                <ChevronRight className="ml-2 text-gray-500 h-4" />
                                 <Link
                                     href={page.href}
-                                    className="ml-4 text-sm font-medium text-primary/70 hover:text-primary"
+                                    className="ml-4 text-sm font-medium text-gray-500 hover:text-primary"
                                 >
                                     {page.name}
                                 </Link>
