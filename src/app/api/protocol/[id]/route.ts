b16fd2d59393972443b0f21a1111b426963f2159
amilcarrey <amilcarrey.ar@gmail.com>
2023-03-20 23:46:08 -0300

    Moving PUT to route handler

---
+++ src/app/api/protocol/[id]/route.ts
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @next/next/no-server-import-in-page
+import { NextRequest, NextResponse } from 'next/server'
+import { updateProtocolById } from '@repositories/protocol'
+
+export async function PUT(request: NextRequest, { params }: any) { //I cannot find the type for params https://beta.nextjs.org/docs/routing/route-handlers#dynamic-route-segments
+   const id = params.id
+   const protocol = await request.json()
+   if (protocol) delete protocol.id
+   const updated = await updateProtocolById(id, protocol)
+   if (!updated) {
+      return NextResponse.error() //How we can add a custom message?
+   }
+   return NextResponse.json({ sucess: true })
+}
