6944cdaf1a57c34490d17fd027a087a151d17d5d
amilcarrey <amilcarrey.ar@gmail.com>
2023-03-21 01:29:07 -0300

    Moving users to route handler

---
+++ src/app/api/users/[id]/route.ts
@@ -0,0 +1,21 @@
+// eslint-disable-next-line @next/next/no-server-import-in-page
+import { NextRequest, NextResponse } from 'next/server'
+import { updateUserById } from '@repositories/users'
+
+export async function PUT(request: NextRequest, { params }: any) {
+   const id = params.id
+   const { role } = await request.json()
+
+   if (!id || !role) {
+      return new Response('We cannot update your user: Invalid Data', { status: 500 })
+   }
+
+   const updated = await updateUserById(id as string, { role })
+
+   if (!updated) {
+      return new Response('We cannot update your user', { status: 500 })
+   }
+
+   return NextResponse.json({ message: "success" })
+}
+
