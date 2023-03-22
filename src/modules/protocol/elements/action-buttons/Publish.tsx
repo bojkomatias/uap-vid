4853c0f94b2c0eea0af4e9da1fd3ceffbbb4da06
amilcarrey <amilcarrey.ar@gmail.com>
2023-03-22 15:28:53 -0300

    Publish button

--- src/modules/protocol/elements/action-buttons/Publish.tsx
+++ src/modules/protocol/elements/action-buttons/Publish.tsx
@@ -4,25 +4,46 @@ import { Protocol } from '@prisma/client'
 import { canExecute } from '@utils/scopes'
 import { ACTION, ProtocolSchema, RoleType, StateType } from '@utils/zod'
 import { useMemo } from 'react'
+import { useNotifications } from '@mantine/notifications'
 
 type ActionButtonTypes = { role: RoleType; protocol: Protocol }
 
-const publishProtocol = async () => {
-    const res = await fetch(``)
-}
-
 export default function PublishButton({ role, protocol }: ActionButtonTypes) {
+    const notification = useNotifications()
+    const publishProtocol = async () => {
+        const published = await fetch('/api/protocols/state/publish', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ id: protocol.id }),
+        })
+        if (published.ok) {
+            notification.showNotification({
+                title: 'Protocolo publicado',
+                message: 'El protocolo ha sido publicado con éxito',
+                color: 'green',
+            })
+        }
+        notification.showNotification({
+            title: 'No hemos podido publicar el protocolo',
+            message: 'Lo lamentamos, ha ocurrido un error',
+            color: 'red',
+        })
+    }
     const isValid = useMemo(
         () => ProtocolSchema.safeParse(protocol).success,
         [protocol]
     )
     if (!protocol.id || !canExecute(ACTION.PUBLISH, role, protocol.state))
         return <></>
-    if (!isValid)
-        return (
-            <Button intent={'primary'} disabled>
-                Publicar
-            </Button>
-        )
-    return <Button intent={'primary'}>Publicar</Button>
+    return (
+        <Button
+            onClick={publishProtocol}
+            intent={'primary'}
+            disabled={!isValid}
+        >
+            Publicar
+        </Button>
+    )
 }
