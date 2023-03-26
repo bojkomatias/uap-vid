789cbbdf6a9699b9ec98d01f5e5cf0eb9cac643b
bojkomatias <bojko.matias@gmail.com>
2023-03-26 17:14:59 -0300

    Added dynamic imports

--- src/modules/protocol/View/index.tsx
+++ src/modules/protocol/View/index.tsx
@@ -1,12 +1,27 @@
 import type { Protocol, Role } from '@prisma/client'
-import IdentificationView from '@protocol/View/Sections/IdentificationView'
-import BibliographyView from './Sections/BibliographyView'
-import BudgetView from './Sections/BudgetView'
-import DescriptionSection from './Sections/DescriptionView'
-import DurationView from './Sections/DurationView'
-import IntroductionView from './Sections/IntroductionView'
-import MethodologyView from './Sections/MethodologyView'
-import PublicationView from './Sections/PublicationView'
+import dynamic from 'next/dynamic'
+const IdentificationView = dynamic(
+    () => import('@protocol/View/Sections/IdentificationView')
+)
+const BibliographyView = dynamic(
+    () => import('@protocol/View/Sections/BibliographyView')
+)
+const DescriptionView = dynamic(
+    () => import('@protocol/View/Sections/DescriptionView')
+)
+const BudgetView = dynamic(() => import('@protocol/View/Sections/BudgetView'))
+const DurationView = dynamic(
+    () => import('@protocol/View/Sections/DurationView')
+)
+const IntroductionView = dynamic(
+    () => import('@protocol/View/Sections/IntroductionView')
+)
+const MethodologyView = dynamic(
+    () => import('@protocol/View/Sections/MethodologyView')
+)
+const PublicationView = dynamic(
+    () => import('@protocol/View/Sections/PublicationView')
+)
 
 export default function View({
     protocol,
@@ -20,14 +35,14 @@ export default function View({
             <div className="px-4">
                 <IdentificationView data={protocol.sections.identification} />
                 <DurationView data={protocol.sections.duration} />
-                <DescriptionSection data={protocol.sections.description} />
+                <DescriptionView data={protocol.sections.description} />
                 <MethodologyView data={protocol.sections.methodology} />
             </div>
         )
     if (role === 'SCIENTIST')
         return (
             <div className="px-4">
-                <DescriptionSection data={protocol.sections.description} />
+                <DescriptionView data={protocol.sections.description} />
                 <IntroductionView data={protocol.sections.introduction} />
                 <PublicationView data={protocol.sections.publication} />
                 <BibliographyView data={protocol.sections.bibliography} />
@@ -38,7 +53,7 @@ export default function View({
             <IdentificationView data={protocol.sections.identification} />
             <DurationView data={protocol.sections.duration} />
             <BudgetView data={protocol.sections.budget} />
-            <DescriptionSection data={protocol.sections.description} />
+            <DescriptionView data={protocol.sections.description} />
             <IntroductionView data={protocol.sections.introduction} />
             <MethodologyView data={protocol.sections.methodology} />
             <PublicationView data={protocol.sections.publication} />
