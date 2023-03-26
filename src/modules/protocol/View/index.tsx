c74170c51b0aa86733f18030a8eb4b1b513bfc9a
bojkomatias <bojko.matias@gmail.com>
2023-03-26 14:44:53 -0300

    Limit view for evaluators

--- src/modules/protocol/View/index.tsx
+++ src/modules/protocol/View/index.tsx
@@ -1,4 +1,4 @@
-import type { Protocol } from '@prisma/client'
+import type { Protocol, Role } from '@prisma/client'
 import IdentificationView from '@protocol/View/Sections/IdentificationView'
 import BibliographyView from './Sections/BibliographyView'
 import BudgetView from './Sections/BudgetView'
@@ -8,7 +8,31 @@ import IntroductionView from './Sections/IntroductionView'
 import MethodologyView from './Sections/MethodologyView'
 import PublicationView from './Sections/PublicationView'
 
-export default function View({ protocol }: { protocol: Protocol }) {
+export default function View({
+    protocol,
+    role,
+}: {
+    protocol: Protocol
+    role: Role
+}) {
+    if (role === 'METHODOLOGIST')
+        return (
+            <div className="px-4">
+                <IdentificationView data={protocol.sections.identification} />
+                <DurationView data={protocol.sections.duration} />
+                <DescriptionSection data={protocol.sections.description} />
+                <MethodologyView data={protocol.sections.methodology} />
+            </div>
+        )
+    if (role === 'SCIENTIST')
+        return (
+            <div className="px-4">
+                <DescriptionSection data={protocol.sections.description} />
+                <IntroductionView data={protocol.sections.introduction} />
+                <PublicationView data={protocol.sections.publication} />
+                <BibliographyView data={protocol.sections.bibliography} />
+            </div>
+        )
     return (
         <div className="px-4">
             <IdentificationView data={protocol.sections.identification} />
