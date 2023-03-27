e5b1a35608ba944193bb61056f2587d4be3e19d0
bojkomatias <bojko.matias@gmail.com>
2023-03-26 22:14:35 -0300

    added info to review visualization

--- src/modules/protocol/elements/TipTapViewer.tsx
+++ src/modules/protocol/elements/TipTapViewer.tsx
@@ -2,12 +2,18 @@
 import TextAlign from '@tiptap/extension-text-align'
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
+import clsx from 'clsx'
 
 interface TipTapViewerProps {
     title: string
     content: string | null
+    rounded?: boolean
 }
-const TipTapViewer = ({ title, content }: TipTapViewerProps) => {
+const TipTapViewer = ({
+    title,
+    content,
+    rounded = true,
+}: TipTapViewerProps) => {
     const editor = useEditor({
         extensions: [
             StarterKit,
@@ -22,7 +28,11 @@ const TipTapViewer = ({ title, content }: TipTapViewerProps) => {
     return (
         <div className="sm:col-span-2">
             <dt className="text-sm font-medium text-gray-500">{title}</dt>
-            <dd className="border p-6 rounded max-w-none prose">
+            <dd
+                className={clsx('border px-4 max-w-none prose', {
+                    rounded: rounded,
+                })}
+            >
                 <EditorContent editor={editor} />
             </dd>
         </div>
