d0c289ef5757de4ae5032b2cae71a92fbf60b099
Nicolás Horn <nicoskate000@gmail.com>
2023-03-28 03:06:05 -0300

    added search bar

--- src/app/protocols/page.tsx
+++ src/app/protocols/page.tsx
@@ -5,11 +5,15 @@ import { getServerSession } from 'next-auth'
 import { redirect } from 'next/navigation'
 import { authOptions } from 'pages/api/auth/[...nextauth]'
 import {
+    getProtocolsWithoutPagination,
     getProtocolByRol,
     getTotalRecordsProtocol,
 } from 'repositories/protocol'
-
+import { Button } from '@elements/Button'
 import Pagination from '@elements/Pagination'
+import SearchBar from '@elements/SearchBar'
+import fuzzysort from 'fuzzysort'
+import { Protocol } from '@prisma/client'
 
 // SSR Server Component, so no need to fetch from api endpoint
 export default async function Page({ searchParams }: { searchParams: any }) {
@@ -17,7 +21,7 @@ export default async function Page({ searchParams }: { searchParams: any }) {
     if (!session) return redirect('/login')
     const protocolCount = await getTotalRecordsProtocol()
 
-    const protocols = session.user
+    const protocolsPaginated = session.user
         ? await getProtocolByRol(
               session.user.role,
               session.user.id,
@@ -25,24 +29,56 @@ export default async function Page({ searchParams }: { searchParams: any }) {
           )
         : []
 
+    //Problema a resolver, traer resultados condicionalmente. Ahora mismo está trayendo tanto los protocolos paginados como todos los protocolos, solo que los muestra condicionalmente. Creo que se resuelve fácil pero para poder probar rápido la búsqueda estoy haciendo chanchadas.
+    const protocols = session.user
+        ? await getProtocolsWithoutPagination(
+              session.user.role,
+              session.user.id
+          )
+        : []
+
+    /* This is the function that performs the search. Uses fuzzysort library. In the keys array you can put whatever key/s you want the search to be perfomed onto**/
+    const searchedProtocols = (): Protocol[] => {
+        const results = fuzzysort.go(searchParams.search, protocols as any, {
+            keys: [
+                'sections.identification.title',
+                'sections.identification.career',
+                'sections.identification.assignment',
+                'sections.identification.sponsor',
+            ],
+        })
+        return results.map((result) => {
+            return result.obj as Protocol
+        })
+    }
+
     return (
         <>
             <Heading title="Lista de proyectos de investigación" />
-            <p className="ml-4 mt-2 text-sm text-gray-500">
+            <p className="mt-2 text-sm text-gray-500">
                 Lista de todos los protocolos cargados en el sistema, haz click
                 en &apos;ver&apos; para más detalles.
             </p>
 
-            <div className="flex justify-end">
+            <div className="mt-3 flex justify-between">
+                <SearchBar />
                 <CreateButton role={session?.user?.role!} />
             </div>
 
-            <Table items={protocols} />
-            <Pagination
-                pageParams={Number(searchParams.page) || 1}
-                count={protocolCount}
-                href="/protocols"
+            <Table
+                items={
+                    searchParams.search
+                        ? searchedProtocols()
+                        : protocolsPaginated
+                }
             />
+            {searchParams.search ? null : (
+                <Pagination
+                    pageParams={Number(searchParams.page) || 1}
+                    count={protocolCount}
+                    href="/protocols"
+                />
+            )}
         </>
     )
 }
---
+++ src/modules/elements/SearchBar.tsx
@@ -0,0 +1,48 @@
+'use client'
+import { Button } from './Button'
+import { KeyboardEvent, SyntheticEvent, useState } from 'react'
+import { useRouter } from 'next/navigation'
+
+export default function SearchBar() {
+    const [searchQuery, setSearchQuery] = useState('')
+    const router = useRouter()
+
+    function handleKeyPress(e: KeyboardEvent) {
+        if (e.key === 'Enter') {
+            if (searchQuery == '') {
+                router.push('/protocols')
+            } else {
+                router.push(`/protocols?search=${searchQuery}`)
+            }
+        }
+    }
+
+    console.log(searchQuery)
+
+    return (
+        <div className="mr-2  flex flex-grow items-center gap-2 rounded-md">
+            <input
+                onKeyPress={(e) => {
+                    handleKeyPress(e as KeyboardEvent)
+                }}
+                onChange={(e) => {
+                    setSearchQuery(e.target.value)
+                }}
+                className="input"
+                placeholder="Buscar protocolo por título, facultad, carrera, estado, etc."
+            />
+            <Button
+                onClick={() => {
+                    if (searchQuery == '') {
+                        router.push('/protocols')
+                    } else {
+                        router.push(`/protocols?search=${searchQuery}`)
+                    }
+                }}
+                intent="secondary"
+            >
+                Buscar
+            </Button>
+        </div>
+    )
+}
