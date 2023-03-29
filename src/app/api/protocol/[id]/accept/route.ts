434d9af63cf0277c031fe40a7e82e2f5efceab3e
amilcarrey <amilcarrey.ar@gmail.com>
2023-03-29 15:24:00 -0300

    Accept protocol button

---
+++ src/app/api/protocol/[id]/accept/route.ts
@@ -0,0 +1,18 @@
+// eslint-disable-next-line @next/next/no-server-import-in-page
+import { NextRequest, NextResponse } from 'next/server'
+import { changeProtocolState } from '@repositories/protocol'
+import { State } from '@prisma/client'
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const id = params.id
+  const protocol = await request.json()
+  if (protocol) delete protocol.id
+  const updated = await changeProtocolState(id, State.ACCEPTED)
+  if (!updated) {
+    return new Response('We cannot accept this protocol', { status: 500 })
+  }
+  return NextResponse.json({ success: true })
+}
