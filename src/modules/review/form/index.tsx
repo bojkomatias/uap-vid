c9b28f6f790f7e71d0a04794977a284a97d17e51
bojkomatias <bojko.matias@gmail.com>
2023-03-30 11:06:21 -0300

    Inject direct form.values into 'preview'

--- src/modules/review/form/index.tsx
+++ src/modules/review/form/index.tsx
@@ -88,118 +88,121 @@ export default function ReviewForm({
                 fullWidth
                 transitionDuration={300}
             />
-            {editing === '0' ? (
-                <ul>
-                    <ReviewItem review={review} user={review.reviewer} />
-                </ul>
-            ) : (
-                <form
-                    onSubmit={(e) => {
-                        e.preventDefault()
-                        addReview(form.values.data, form.values.verdict)
-                    }}
-                >
-                    <label className="label">Observación</label>
-                    <Tiptap {...form.getInputProps('data')} />
-                    {form.getInputProps('data').error ? (
-                        <p className=" pl-3 pt-1 text-xs text-gray-600 saturate-[80%]">
-                            *{form.getInputProps('data').error}
-                        </p>
-                    ) : null}
 
-                    <RadioGroup
-                        {...form.getInputProps('verdict')}
-                        defaultValue="PENDING"
-                    >
-                        <RadioGroup.Label className="label">
-                            Veredicto
-                        </RadioGroup.Label>
-                        <div className="-space-y-px bg-white">
-                            {Object.entries(ReviewVerdictsDictionary).map(
-                                ([id, name], index) => (
-                                    <RadioGroup.Option
-                                        key={id}
-                                        value={id}
-                                        className={({ checked }) =>
-                                            clsx(
-                                                index === 0
-                                                    ? 'rounded-tl rounded-tr'
-                                                    : '',
-                                                index ===
-                                                    Object.keys(
-                                                        ReviewVerdictsDictionary
-                                                    ).length -
-                                                        1
-                                                    ? 'rounded-bl rounded-br'
-                                                    : '',
-                                                checked
-                                                    ? 'z-10 border-gray-300 bg-primary/5'
-                                                    : 'border-gray-200',
-                                                'relative flex cursor-pointer items-baseline border px-5 py-2.5 focus:outline-none'
-                                            )
-                                        }
-                                    >
-                                        {({ active, checked }) => (
-                                            <>
-                                                <span
+            <ul className={editing === '0' ? 'block' : 'hidden'}>
+                <ReviewItem
+                    review={{ ...form.values, reviewer: review.reviewer }}
+                    user={review.reviewer}
+                />
+            </ul>
+
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault()
+                    addReview(form.values.data, form.values.verdict)
+                }}
+                className={editing === '1' ? 'block' : 'hidden'}
+            >
+                <label className="label">Observación</label>
+                <Tiptap {...form.getInputProps('data')} />
+                {form.getInputProps('data').error ? (
+                    <p className=" pl-3 pt-1 text-xs text-gray-600 saturate-[80%]">
+                        *{form.getInputProps('data').error}
+                    </p>
+                ) : null}
+
+                <RadioGroup
+                    {...form.getInputProps('verdict')}
+                    defaultValue="PENDING"
+                >
+                    <RadioGroup.Label className="label">
+                        Veredicto
+                    </RadioGroup.Label>
+                    <div className="-space-y-px bg-white">
+                        {Object.entries(ReviewVerdictsDictionary).map(
+                            ([id, name], index) => (
+                                <RadioGroup.Option
+                                    key={id}
+                                    value={id}
+                                    className={({ checked }) =>
+                                        clsx(
+                                            index === 0
+                                                ? 'rounded-tl rounded-tr'
+                                                : '',
+                                            index ===
+                                                Object.keys(
+                                                    ReviewVerdictsDictionary
+                                                ).length -
+                                                    1
+                                                ? 'rounded-bl rounded-br'
+                                                : '',
+                                            checked
+                                                ? 'z-10 border-gray-300 bg-primary/5'
+                                                : 'border-gray-200',
+                                            'relative flex cursor-pointer items-baseline border px-5 py-2.5 focus:outline-none'
+                                        )
+                                    }
+                                >
+                                    {({ active, checked }) => (
+                                        <>
+                                            <span
+                                                className={clsx(
+                                                    checked
+                                                        ? 'border-transparent bg-primary'
+                                                        : 'border-gray-300 bg-white',
+                                                    active
+                                                        ? 'ring-2 ring-primary ring-offset-1'
+                                                        : '',
+                                                    'flex h-4 w-4 shrink-0 cursor-pointer items-center justify-center rounded-full border'
+                                                )}
+                                                aria-hidden="true"
+                                            >
+                                                <span className="h-1.5 w-1.5 rounded-full bg-white" />
+                                            </span>
+                                            <span className="ml-3 flex flex-col">
+                                                <RadioGroup.Label
+                                                    as="span"
                                                     className={clsx(
                                                         checked
-                                                            ? 'border-transparent bg-primary'
-                                                            : 'border-gray-300 bg-white',
-                                                        active
-                                                            ? 'ring-2 ring-primary ring-offset-1'
-                                                            : '',
-                                                        'flex h-4 w-4 shrink-0 cursor-pointer items-center justify-center rounded-full border'
+                                                            ? 'font-medium text-gray-900'
+                                                            : 'font-regular text-gray-700',
+                                                        'block text-sm'
                                                     )}
-                                                    aria-hidden="true"
                                                 >
-                                                    <span className="h-1.5 w-1.5 rounded-full bg-white" />
-                                                </span>
-                                                <span className="ml-3 flex flex-col">
-                                                    <RadioGroup.Label
-                                                        as="span"
-                                                        className={clsx(
-                                                            checked
-                                                                ? 'font-medium text-gray-900'
-                                                                : 'font-regular text-gray-700',
-                                                            'block text-sm'
-                                                        )}
-                                                    >
-                                                        {name}
-                                                    </RadioGroup.Label>
-                                                    <RadioGroup.Description
-                                                        as="span"
-                                                        className={clsx(
-                                                            checked
-                                                                ? 'text-gray-700'
-                                                                : 'text-gray-500',
-                                                            'block text-xs'
-                                                        )}
-                                                    >
-                                                        {id === 'PENDING'
-                                                            ? 'Enviar correcciones sin veredicto, esperar cambios para re-evaluar.'
-                                                            : id === 'APPROVED'
-                                                            ? 'Hacer devolución del proyecto como válido y apto para continuar el proceso.'
-                                                            : 'Marcar proyecto como rechazado.'}
-                                                    </RadioGroup.Description>
-                                                </span>
-                                            </>
-                                        )}
-                                    </RadioGroup.Option>
-                                )
-                            )}
-                        </div>
-                    </RadioGroup>
+                                                    {name}
+                                                </RadioGroup.Label>
+                                                <RadioGroup.Description
+                                                    as="span"
+                                                    className={clsx(
+                                                        checked
+                                                            ? 'text-gray-700'
+                                                            : 'text-gray-500',
+                                                        'block text-xs'
+                                                    )}
+                                                >
+                                                    {id === 'PENDING'
+                                                        ? 'Enviar correcciones sin veredicto, esperar cambios para re-evaluar.'
+                                                        : id === 'APPROVED'
+                                                        ? 'Hacer devolución del proyecto como válido y apto para continuar el proceso.'
+                                                        : 'Marcar proyecto como rechazado.'}
+                                                </RadioGroup.Description>
+                                            </span>
+                                        </>
+                                    )}
+                                </RadioGroup.Option>
+                            )
+                        )}
+                    </div>
+                </RadioGroup>
 
-                    <Button
-                        type="submit"
-                        className="ml-auto mt-2"
-                        intent="secondary"
-                    >
-                        Comentar
-                    </Button>
-                </form>
-            )}
+                <Button
+                    type="submit"
+                    className="ml-auto mt-2"
+                    intent="secondary"
+                >
+                    Comentar
+                </Button>
+            </form>
         </ItemContainer>
     )
 }
