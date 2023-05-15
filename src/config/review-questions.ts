d8f0b2f41de46aa8f5e5acc55ea9ca2836e8ad70
bojkomatias <bojko.matias@gmail.com>
2023-05-15 14:00:49 -0300

    Questions done!

--- src/config/review-questions.ts
+++ src/config/review-questions.ts
@@ -74,58 +74,92 @@ export const questions = [
             '¿El proyecto muestra solidez interna, está fundamentado adecuadamente e incluye una revisión bibliográfica pertinente, actualizada y suficiente?',
     },
     {
-        id: '10',
+        id: '11',
         type: 'SCIENTIFIC',
         question:
             '¿Los objetivos propuestos para el proyecto guardan una relación clara con el problema de investigación y con las descripciones y las motivaciones expuestas?',
     },
     {
-        id: '10',
+        id: '12',
         type: 'SCIENTIFIC',
         question:
             '¿Están bien los enunciados del problema de investigación, las hipótesis y los objetivos propuestos?',
     },
     {
-        id: '10',
+        id: '13',
         type: 'SCIENTIFIC',
         question:
             '¿En qué medida el tema de investigación expone un grado aceptable de originalidad y, por lo tanto, contribuirá al avance del conocimiento científico?',
     },
     {
-        id: '10',
+        id: '14',
         type: 'SCIENTIFIC',
         question:
             '¿Cómo estima que será la contribución de esta investigación a la formación de recursos humanos capacitados para la investigación?',
     },
     {
-        id: '10',
+        id: '15',
         type: 'SCIENTIFIC',
         question:
             '¿Cómo valora la contribución que esta investigación podría realizar para el desarrollo de la sociedad de la región en aspectos tales como el económico, el social, el cultural, el tecnológico o la salud?',
     },
     {
-        id: '10',
+        id: '16',
         type: 'SCIENTIFIC',
         question:
             'En relación con las perspectivas de transferencia de los resultados: ¿Estos son adecuados?',
     },
     {
-        id: '10',
+        id: '17',
         type: 'SCIENTIFIC',
         question:
             'En relación con las perspectivas de transferencia de los resultados: ¿Tienen una relación clara con los objetivos y las demás secciones del proyecto de investigación?',
     },
     {
-        id: '10',
+        id: '18',
         type: 'SCIENTIFIC',
         question:
             'En relación con las perspectivas de transferencia de los resultados: ¿Los beneficios esperados están expresados con claridad?',
     },
     {
-        id: '10',
+        id: '19',
         type: 'SCIENTIFIC',
         question:
             '¿El estudio tiene a su criterio tiene posibilidades de ser publicado en revistas científicas indexadas o en un libro?',
     },
-    // previo al punto 3 (mail enviado clarificando director y presupuesto.)
+    {
+        id: '20',
+        type: 'SCIENTIFIC',
+        question:
+            'Los antecedentes del director ¿Son suficientes para para desarrollar el plan de trabajo propuesto, sobre la base de su formación académica, su experiencia y la calidad de su labor en investigación?',
+    },
+    {
+        id: '21',
+        type: 'SCIENTIFIC',
+        question:
+            'Los antecedentes del codirector ¿Son suficientes para para desarrollar el plan de trabajo propuesto, sobre la base de su formación académica, su experiencia y la calidad de su labor en investigación?',
+    },
+    {
+        id: '22',
+        type: 'SCIENTIFIC',
+        question:
+            '¿El cronograma se ajusta a las acciones de investigación propuestas?',
+    },
+    {
+        id: '23',
+        type: 'SCIENTIFIC',
+        question:
+            '¿Los insumos, los materiales y los reactivos incluidos en el anteproyecto son adecuados para la realización del proyecto?',
+    },
+    {
+        id: '24',
+        type: 'SCIENTIFIC',
+        question: '¿El presupuesto de gastos directos es adecuado?',
+    },
+    {
+        id: '25',
+        type: 'SCIENTIFIC',
+        question:
+            '¿El presupuesto de tiempo presentado responde a las demandas del proyecto?',
+    },
 ]
