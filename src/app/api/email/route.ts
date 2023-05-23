ba30edd13b4b1aef3e06907870ddc646e7f9bd2e
Nico Horn <contact@nicohorn.com>
2023-05-22 22:14:28 -0300

    hola

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -1,13 +1,11 @@
 /* eslint-disable @next/next/no-server-import-in-page */
 
-import { getProtocolWithResearcher } from '@repositories/protocol'
 import type { NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
 import nodemailer from 'nodemailer'
 
 export async function POST(request: NextRequest) {
-    const { from, to, subject, message, template, protocolId } =
-        await request.json()
+    const { from, to, subject, message, template } = await request.json()
 
     const transporter = nodemailer.createTransport({
         host: 'smtp.gmail.com',
@@ -18,9 +16,6 @@ export async function POST(request: NextRequest) {
         },
     })
 
-    const protocol = getProtocolWithResearcher(protocolId)
-
-    console.log(protocol)
     const emailObject = {
         from: from,
         to: to,
