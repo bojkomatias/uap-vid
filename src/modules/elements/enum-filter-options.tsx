8b0630eea551668245a4365689ae5324a2c93616
bojkomatias <bojko.matias@gmail.com>
2023-08-20 20:28:07 -0300

    minor changes

--- src/modules/elements/enum-filter-options.tsx
+++ src/modules/elements/enum-filter-options.tsx
@@ -18,43 +18,41 @@ export default function EnumFilterOptions({
     return (
         <div>
             <div className="relative mt-4 flex flex-col items-start text-sm">
-                <div className="relative flex gap-2">
-                    <div className="flex gap-2">
-                        {values.map(([value, name], i) => {
-                            return (
-                                <Button
-                                    onClick={() => {
-                                        update({
-                                            filter,
-                                            values: currentValues
-                                                ? currentValues.includes(value)
-                                                    ? currentValues
-                                                          .filter(
-                                                              (e) => e !== value
-                                                          )
-                                                          .join('-')
-                                                    : currentValues
-                                                          .join('-')
-                                                          .concat('-', value)
-                                                : value,
-                                        })
-                                    }}
-                                    intent="unset"
-                                    key={i}
+                <div className="relative flex flex-wrap gap-2">
+                    {values.map(([value, name], i) => {
+                        return (
+                            <Button
+                                onClick={() => {
+                                    update({
+                                        filter,
+                                        values: currentValues
+                                            ? currentValues.includes(value)
+                                                ? currentValues
+                                                      .filter(
+                                                          (e) => e !== value
+                                                      )
+                                                      .join('-')
+                                                : currentValues
+                                                      .join('-')
+                                                      .concat('-', value)
+                                            : value,
+                                    })
+                                }}
+                                intent="unset"
+                                key={i}
+                            >
+                                <Badge
+                                    className={cx(
+                                        'cursor-pointer transition hover:bg-gray-100',
+                                        currentValues?.includes(value) &&
+                                            'bg-gray-300'
+                                    )}
                                 >
-                                    <Badge
-                                        className={cx(
-                                            'cursor-pointer hover:bg-gray-100',
-                                            currentValues?.includes(value) &&
-                                                'bg-gray-200 '
-                                        )}
-                                    >
-                                        {name}
-                                    </Badge>
-                                </Button>
-                            )
-                        })}
-                    </div>
+                                    {name}
+                                </Badge>
+                            </Button>
+                        )
+                    })}
                 </div>
             </div>
         </div>
--- src/modules/layout/page-heading.tsx
+++ src/modules/layout/page-heading.tsx
@@ -1,7 +1,7 @@
 import type { ReactNode } from 'react'
 
 export const PageHeading = ({ title }: { title: string | ReactNode }) => (
-    <div className="ml-2 mt-16">
+    <div className="mx-auto mt-16 max-w-7xl pl-2">
         <h2 className="text-2xl font-bold leading-10 text-black/70">{title}</h2>
     </div>
 )
