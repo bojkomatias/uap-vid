e6150b30b2e11847f12fd239a0a743fc9a697343
bojkomatias <bojko.matias@gmail.com>
2023-08-20 18:23:05 -0300

    breakpoints

--- src/modules/protocol/protocol-metadata.tsx
+++ src/modules/protocol/protocol-metadata.tsx
@@ -26,38 +26,40 @@ export async function ProtocolMetadata({
         researcherList = await getAllResearchers()
     }
     return (
-        <div className="ml-2 mt-1 max-w-2xl rounded-lg bg-gray-50/50 px-3 py-2 leading-loose drop-shadow-sm">
-            <div className="flex items-center justify-between gap-4">
-                <div className="flex items-center gap-2">
-                    <Calendar className="h-4 text-gray-600" />
-                    <div className="mt-0.5 text-xs text-gray-600">
-                        {dateFormatter.format(createdAt)}
-                    </div>
-                </div>
-                <Badge>{convocatory.name}</Badge>
-            </div>
-            <div className="mt-2 flex items-baseline">
-                <div className="flex items-center gap-2">
-                    <UserIcon className="h-4 text-gray-600" />
-                    <div className="font-medium">
-                        {researcher.name}
-                        <div className="-mt-2 ml-px text-xs font-light text-gray-500">
-                            {researcher.email}
+        <div className="mx-auto max-w-7xl">
+            <div className="ml-2 mt-1 max-w-2xl rounded-lg bg-gray-50/50 px-3 py-2 leading-loose drop-shadow-sm">
+                <div className="flex items-center justify-between gap-4">
+                    <div className="flex items-center gap-2">
+                        <Calendar className="h-4 text-gray-600" />
+                        <div className="mt-0.5 text-xs text-gray-600">
+                            {dateFormatter.format(createdAt)}
                         </div>
                     </div>
+                    <Badge>{convocatory.name}</Badge>
                 </div>
-                <div className="mx-3 mt-1 flex-grow">
-                    {currentUser.role === 'ADMIN' && (
-                        <ResearcherReassignation
-                            protocolId={id}
-                            researcherId={researcher.id}
-                            researchers={researcherList}
-                        />
-                    )}
+                <div className="mt-2 flex items-baseline">
+                    <div className="flex items-center gap-2">
+                        <UserIcon className="h-4 text-gray-600" />
+                        <div className="font-medium">
+                            {researcher.name}
+                            <div className="-mt-2 ml-px text-xs font-light text-gray-500">
+                                {researcher.email}
+                            </div>
+                        </div>
+                    </div>
+                    <div className="mx-3 mt-1 flex-grow">
+                        {currentUser.role === 'ADMIN' && (
+                            <ResearcherReassignation
+                                protocolId={id}
+                                researcherId={researcher.id}
+                                researchers={researcherList}
+                            />
+                        )}
+                    </div>
+                    <Badge className="text-sm">
+                        {ProtocolStatesDictionary[state]}
+                    </Badge>
                 </div>
-                <Badge className="text-sm">
-                    {ProtocolStatesDictionary[state]}
-                </Badge>
             </div>
         </div>
     )
