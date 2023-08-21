45683db6ab1a93491c0e9bdc0d57684d05bde6c1
bojkomatias <bojko.matias@gmail.com>
2023-08-20 22:42:48 -0300

    Prisma type

--- src/repositories/protocol.ts
+++ src/repositories/protocol.ts
@@ -158,7 +158,6 @@ const getProtocolsByRol = cache(
                                       is: {
                                           title: {
                                               contains: search,
-                                              mode: 'insensitive',
                                           },
                                       },
                                   },
