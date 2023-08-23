ca419dcf4c78b54f74230ea1c23c12faf11046e3
bojkomatias <bojko.matias@gmail.com>
2023-08-23 02:05:12 -0300

    Nice baby!

--- src/app/(authenticated)/team-members/[id]/page.tsx
+++ src/app/(authenticated)/team-members/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { PageHeading } from '@layout/page-heading'
 import { Role } from '@prisma/client'
+import { getTeamMemberById } from '@repositories/team-member'
 import { getAllOwners } from '@repositories/user'
 import { authOptions } from 'app/api/auth/[...nextauth]/route'
 import TeamMemberForm from 'modules/team-member/team-member-form'
@@ -9,7 +10,8 @@ export default async function Page({ params }: { params: { id: string } }) {
     const session = await getServerSession(authOptions)
     if (!session || session.user.role !== Role.ADMIN) return
 
-    const member = params.id === 'new' ? { categories: [] } : { categories: [] }
+    const member =
+        params.id === 'new' ? null : await getTeamMemberById(params.id)
 
     const researchers = await getAllOwners()
 
---
+++ src/app/api/team-members/[id]/route.ts
@@ -0,0 +1,37 @@
+// eslint-disable-next-line @next/next/no-server-import-in-page
+import { NextResponse, type NextRequest } from 'next/server'
+import {
+    updateCategoryHistory,
+    updateTeamMember,
+} from '@repositories/team-member'
+import { canAccess } from '@utils/scopes'
+import { authOptions } from 'app/api/auth/[...nextauth]/route'
+import { getServerSession } from 'next-auth'
+
+export async function PUT(
+    request: NextRequest,
+    { params }: { params: { id: string } }
+) {
+    const session = await getServerSession(authOptions)
+    if (session && canAccess('TEAM_MEMBERS', session.user.role)) {
+        const teamMember = await request.json()
+        const { id } = params
+        const updated = await updateTeamMember(id, teamMember)
+        if (!updated)
+            return new Response('Failed to create Team Member', { status: 500 })
+        return NextResponse.json(updated)
+    }
+    return new Response('Unauthorized', { status: 401 })
+}
+
+export async function PATCH(request: NextRequest) {
+    const session = await getServerSession(authOptions)
+    if (session && canAccess('TEAM_MEMBERS', session.user.role)) {
+        const data = await request.json()
+        const updated = await updateCategoryHistory(data)
+        if (!updated)
+            return new Response('Failed to update category', { status: 500 })
+        return NextResponse.json(updated)
+    }
+    return new Response('Unauthorized', { status: 401 })
+}
