2292af5a4596d9318f64b59f0bba219be0c47388
Nicolás Horn <nicoskate000@gmail.com>
2023-09-21 16:08:07 -0300

    Removed development aids

--- src/utils/dataToCsv.ts
+++ src/utils/dataToCsv.ts
@@ -4,61 +4,6 @@ import type { ColumnDef } from '@tanstack/react-table'
  * @param data - the data passed to the TanStack table.
  */
 
-const usersData = [
-    {
-        id: 1,
-        name: 'Leanne Graham',
-        username: 'Bret',
-        email: 'Sincere@april.biz',
-        phone: '1-770-736-8031 x56442',
-        website: 'hildegard.org',
-    },
-    {
-        id: 2,
-        name: 'Ervin Howell',
-        username: 'Antonette',
-        email: 'Shanna@melissa.tv',
-        phone: '010-692-6593 x09125',
-        website: 'anastasia.net',
-    },
-    {
-        id: 3,
-        name: 'Clementine Bauch',
-        username: 'Samantha',
-        email: 'Nathan@yesenia.net',
-        phone: '1-463-123-4447',
-        website: 'ramiro.info',
-    },
-    {
-        id: 4,
-        name: 'Patricia Lebsack',
-        username: 'Karianne',
-        email: 'Julianne.OConner@kory.org',
-        phone: '493-170-9623 x156',
-        website: 'kale.biz',
-    },
-    {
-        id: 5,
-        name: 'Chelsey Dietrich',
-        username: 'Kamren',
-        email: 'Lucio_Hettinger@annie.ca',
-        phone: '(254)954-1289',
-        website: 'demarco.info',
-    },
-]
-
-const csvData = [
-    ['ID', 'Name', 'Username', 'Email', 'Phone', 'Website'],
-    ...usersData.map(({ id, name, username, email, phone, website }) => [
-        id,
-        name,
-        username,
-        email,
-        phone,
-        website,
-    ]),
-]
-
 export default function dataToCsv(
     columns: ColumnDef<any, unknown>[],
     data: unknown[]
@@ -90,7 +35,7 @@ export default function dataToCsv(
                 }
 
                 // Assign the extracted data to the corresponding key in the row object
-                rowData[c] = JSON.stringify(result)
+                rowData[c] = result.toString()
             })
 
             results.push(rowData) // Push the row object to the results array
@@ -98,10 +43,6 @@ export default function dataToCsv(
 
         return results.map((r) => Object.values(r))
     }
-
-    console.log('AAAAAAAAAAA', [columnsToExport, ...extractedData()])
-
-    console.log('USERS DATA', csvData)
 
     return [columnsToExport, ...extractedData()]
 }
