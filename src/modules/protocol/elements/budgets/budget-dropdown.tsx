ccdb1d2279b41770894c106e614a77059b8d6ee4
Nicolás Horn <nicoskate000@gmail.com>
2023-11-06 19:31:58 -0300

    Added scroll false to Link

--- src/modules/protocol/elements/budgets/budget-dropdown.tsx
+++ src/modules/protocol/elements/budgets/budget-dropdown.tsx
@@ -41,6 +41,7 @@ export function BudgetDropdown({
                             <Menu.Item key={id}>
                                 {({ active }) => (
                                     <Link
+                                        scroll={false}
                                         href={`/anual-budget-view/${id}`}
                                         className={cx(
                                             'flex w-full items-center gap-1 px-4 py-2 text-sm font-medium',
