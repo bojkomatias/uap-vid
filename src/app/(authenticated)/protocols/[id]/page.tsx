8758789731025b31a7a1d180abd4ec80f43774d8
Nicolás Horn <nicoskate000@gmail.com>
2023-11-06 19:31:01 -0300

    Deleted unused import

--- src/app/(authenticated)/protocols/[id]/page.tsx
+++ src/app/(authenticated)/protocols/[id]/page.tsx
@@ -2,7 +2,6 @@ import View from '@protocol/protocol-view-template'
 import { redirect } from 'next/navigation'
 import { findProtocolById } from 'repositories/protocol'
 import { PDF } from 'modules/protocol-pdf'
-import { Suspense } from 'react'
 
 export default async function Page({ params }: { params: { id: string } }) {
     if (params.id === 'new') redirect('/protocols/new/0')
