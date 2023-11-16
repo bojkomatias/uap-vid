4696e35813669e237f9938ec5615eae9bf8d83b1
bojkomatias <bojko.matias@gmail.com>
2023-11-16 19:49:05 -0300

    docs: :memo: Typing and comments
    Added types and comment to route / team-member so not modified, break runtime

--- src/app/api/team-members/[id]/route.ts
+++ src/app/api/team-members/[id]/route.ts
@@ -1,15 +1,20 @@
-
 import { NextResponse, type NextRequest } from 'next/server'
 import {
     updateCategoryHistory,
     updateTeamMember,
 } from '@repositories/team-member'
+import type { TeamMember } from '@prisma/client'
 
 export async function PUT(
     request: NextRequest,
     { params }: { params: { id: string } }
 ) {
-    const { ...teamMember } = await request.json()
+    /**
+     *  Destructured ID so it's not passed on runtime,
+     * prisma doesn't like a malformed ID
+     */
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { id, ...teamMember } = (await request.json()) as TeamMember
     const updated = await updateTeamMember(params.id, teamMember)
     if (!updated)
         return new Response('Failed to create Team Member', { status: 500 })
