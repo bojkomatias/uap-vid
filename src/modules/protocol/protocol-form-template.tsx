6e8107ea4715b1d0fbbb50e54b04a32a6f99485e
bojkomatias <bojko.matias@gmail.com>
2023-11-17 16:24:21 -0300

    Flex wrap on sections

--- src/modules/protocol/protocol-form-template.tsx
+++ src/modules/protocol/protocol-form-template.tsx
@@ -206,7 +206,7 @@ export default function ProtocolForm({ protocol }: { protocol: ProtocolZod }) {
                     initial={{ opacity: 0, y: -7 }}
                     animate={{ opacity: 1, y: 0 }}
                     transition={{ duration: 0.4, delay: 0.4 }}
-                    className="mx-auto mb-6 flex w-fit max-w-full gap-1 overflow-auto rounded border bg-gray-50 p-1"
+                    className="mx-auto mb-6 flex w-fit flex-wrap items-center justify-center gap-1 rounded border bg-gray-50 p-1"
                 >
                     <SectionButton
                         path={'sections.identification'}
