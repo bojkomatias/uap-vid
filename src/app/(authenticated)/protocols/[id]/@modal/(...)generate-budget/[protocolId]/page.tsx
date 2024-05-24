b53c9f7df0e8b1877f36cb57c1bd7557a9ee1c3c
bojkomatias <bojko.matias@gmail.com>
2024-05-24 15:35:45 -0300

    fix

---
+++ src/app/(authenticated)/protocols/[id]/@modal/(...)generate-budget/[protocolId]/page.tsx
@@ -0,0 +1,14 @@
+import { GenerateAnualBudget } from '@protocol/elements/budgets/generate-anual-budget'
+import { ProtocolInterceptedModal } from '@protocol/elements/protocol-intercepted-modal'
+
+export default async function ModalPage({
+    params: { protocolId },
+}: {
+    params: { protocolId: string }
+}) {
+    return (
+        <ProtocolInterceptedModal>
+            <GenerateAnualBudget protocolId={protocolId} />
+        </ProtocolInterceptedModal>
+    )
+}
