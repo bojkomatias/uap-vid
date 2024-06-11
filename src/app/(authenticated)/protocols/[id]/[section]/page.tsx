725418bb4f3a91c94c9dc8c34791b7a509ac0edb
bojkomatias <bojko.matias@gmail.com>
2024-06-10 21:33:19 -0300

    improvemnt:zod only for schema
    Removed all unnecesary redundant types from zod, just use prisma single source of truth

--- src/app/(authenticated)/protocols/[id]/[section]/page.tsx
+++ src/app/(authenticated)/protocols/[id]/[section]/page.tsx
@@ -1,44 +1,44 @@
 import ProtocolForm from '@protocol/protocol-form-template'
 import { initialSectionValues } from '@utils/createContext'
 import { canExecute } from '@utils/scopes'
-import { ACTION, STATE } from '@utils/zod'
 import { getServerSession } from 'next-auth'
 import { redirect } from 'next/navigation'
 import { authOptions } from 'app/api/auth/[...nextauth]/auth'
 import { findProtocolById } from 'repositories/protocol'
+import { Action, ProtocolState } from '@prisma/client'
 
 export default async function Page({
-    params,
-    searchParams,
+  params,
+  searchParams,
 }: {
-    params: { id: string; section: string }
-    searchParams: { convocatory: string }
+  params: { id: string; section: string }
+  searchParams: { convocatory: string }
 }) {
-    const session = await getServerSession(authOptions)
-    if (!session) return
-    if (!searchParams.convocatory && params.id === 'new') redirect('/protocols')
+  const session = await getServerSession(authOptions)
+  if (!session) return
+  if (!searchParams.convocatory && params.id === 'new') redirect('/protocols')
 
-    const protocol =
-        params.id === 'new'
-            ? {
-                  convocatoryId: searchParams.convocatory,
-                  state: STATE.DRAFT,
-                  researcherId: session.user.id,
-                  sections: initialSectionValues,
-              }
-            : await findProtocolById(params.id)
+  const protocol =
+    params.id === 'new'
+      ? {
+        convocatoryId: searchParams.convocatory,
+        state: ProtocolState.DRAFT,
+        researcherId: session.user.id,
+        sections: initialSectionValues,
+      }
+      : await findProtocolById(params.id)
 
-    if (!protocol) redirect('/protocols')
-    if (
-        !canExecute(
-            session.user.id === protocol.researcherId
-                ? ACTION.EDIT_BY_OWNER
-                : ACTION.EDIT,
-            session.user.role,
-            protocol.state
-        )
+  if (!protocol) redirect('/protocols')
+  if (
+    !canExecute(
+      session.user.id === protocol.researcherId
+        ? Action.EDIT_BY_OWNER
+        : Action.EDIT,
+      session.user.role,
+      protocol.state
     )
-        redirect('/protocols')
+  )
+    redirect('/protocols')
 
-    return <ProtocolForm protocol={protocol} />
+  return <ProtocolForm protocol={protocol} />
 }
--- src/modules/review/reviews-template.tsx
+++ src/modules/review/reviews-template.tsx
@@ -1,44 +1,43 @@
 import type { Role } from '@prisma/client'
-import { State } from '@prisma/client'
+import { Access, Action, ProtocolState } from '@prisma/client'
 import { canAccess, canExecute } from '@utils/scopes'
-import { ACCESS, ACTION } from '@utils/zod'
 import ReviewList from './elements/review-list'
 import ReviewFormTemplate from './review-form-template'
 import { cx } from '@utils/cx'
 
 // Component serves as Semaphore for reviews (Assign/Create, AddReview, Visualize)
 export default async function ReviewsTemplate({
-    id,
-    researcherId,
-    state,
-    userId,
-    userRole,
+  id,
+  researcherId,
+  state,
+  userId,
+  userRole,
 }: {
-    id: string
-    researcherId: string
-    state: State
-    userId: string
-    userRole: Role
+  id: string
+  researcherId: string
+  state: ProtocolState
+  userId: string
+  userRole: Role
 }) {
-    return (
-        <aside
-            className={cx(
-                'min-w-fit space-y-3 overflow-y-auto py-3 pl-4 pr-1 lg:sticky lg:top-0 lg:h-screen lg:pl-0',
-                (state === State.DRAFT || state === State.PUBLISHED) && 'hidden'
-            )}
-        >
-            {userId !== researcherId &&
-            canExecute(ACTION.REVIEW, userRole, state) ? (
-                <ReviewFormTemplate protocolId={id} userId={userId} />
-            ) : null}
+  return (
+    <aside
+      className={cx(
+        'min-w-fit space-y-3 overflow-y-auto py-3 pl-4 pr-1 lg:sticky lg:top-0 lg:h-screen lg:pl-0',
+        (state === ProtocolState.DRAFT || state === ProtocolState.PUBLISHED) && 'hidden'
+      )}
+    >
+      {userId !== researcherId &&
+        canExecute(Action.REVIEW, userRole, state) ? (
+        <ReviewFormTemplate protocolId={id} userId={userId} />
+      ) : null}
 
-            {userId === researcherId || canAccess(ACCESS.REVIEWS, userRole) ? (
-                <ReviewList
-                    role={userRole}
-                    id={id}
-                    isOwner={userId === researcherId}
-                />
-            ) : null}
-        </aside>
-    )
+      {userId === researcherId || canAccess(Access.REVIEWS, userRole) ? (
+        <ReviewList
+          role={userRole}
+          id={id}
+          isOwner={userId === researcherId}
+        />
+      ) : null}
+    </aside>
+  )
 }
