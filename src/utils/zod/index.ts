7ba614e020602ea30fd77ec5208150eb2b344eba
nicohorn <nicoskate000@gmail.com>
2024-06-13 20:25:53 -0300

    Cherry picked data model and solved conflicts

--- src/utils/zod/index.ts
+++ src/utils/zod/index.ts
@@ -154,6 +154,10 @@ export const HistoricCategoryPriceSchema = z.object({
   currency: z.string().default('ARS'),
 })
 
+const AmountIndexSchema = z
+  .object({ FCA: z.number(), FMR: z.number() })
+  .nullable()
+
 /////////////////////////////////////////
 // TEAM MEMBER CATEGORY SCHEMA
 // Contiene el array histórico de categorías
@@ -165,6 +169,7 @@ export const TeamMemberCategorySchema = z.object({
   price: HistoricCategoryPriceSchema.array().min(1, {
     message: 'Configure un precio',
   }),
+  amountIndex: AmountIndexSchema, //Remove nullable
 })
 
 /////////////////////////////////////////
