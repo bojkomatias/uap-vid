ffe83338b83f09a27156285951a224778319a1eb
bojkomatias <bojko.matias@gmail.com>
2024-06-15 20:12:52 -0300

    Make new-convocatory-dialog and remove routes for this

--- src/app/(authenticated)/convocatories/page.tsx
+++ src/app/(authenticated)/convocatories/page.tsx
@@ -1,12 +1,11 @@
 import { ConvocatoryTable } from '@convocatory/convocatory-table'
 import { PageHeading } from '@layout/page-heading'
-import Link from 'next/link'
 import { CalendarPlus } from 'tabler-icons-react'
-import { buttonStyle } from '@elements/button/styles'
 import {
   getAllConvocatories,
   getCurrentConvocatory,
 } from '@repositories/convocatory'
+import { NewConvocatoryDialog } from '@convocatory/new-convocatory-dialog'
 
 export default async function Page({
   searchParams,
@@ -19,16 +18,8 @@ export default async function Page({
   return (
     <>
       <PageHeading title="Lista de convocatorias" />
-      <div className="flex flex-row-reverse">
-        <Link
-          scroll={false}
-          href={'/convocatories/new'}
-          className={buttonStyle('secondary')}
-        >
-          <CalendarPlus className="h-4 w-4 text-current" />
-          Nueva convocatoria
-        </Link>
-      </div>
+      {/* New convocatory Dialog */}
+      <NewConvocatoryDialog />
       <ConvocatoryTable
         totalRecords={totalRecords}
         convocatories={convocatories}
---
+++ src/modules/convocatory/new-convocatory-dialog.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import { Dialog, DialogDescription, DialogTitle } from '@components/dialog'
+import { ConvocatoryForm } from './convocatory-form'
+import { useState } from 'react'
+import { CalendarPlus } from 'tabler-icons-react'
+import { Button } from '@components/button'
+
+export function NewConvocatoryDialog() {
+  const [open, setOpen] = useState(false)
+
+  return (
+    <>
+      <Button onClick={() => setOpen(true)} color="blue">
+        <CalendarPlus data-slot="icon" />
+        Convocatoria
+      </Button>
+
+      <Dialog open={open} onClose={setOpen} size="xl">
+        <DialogTitle>Crear convocatoria</DialogTitle>
+        <DialogDescription>
+          Aquí puede crear una nueva convocatoria a cual asignar proyectos de
+          investigación
+        </DialogDescription>
+        <ConvocatoryForm
+          convocatory={{
+            name: '',
+            from: new Date(),
+            to: new Date(),
+            year: new Date().getFullYear(),
+          }}
+        />
+      </Dialog>
+    </>
+  )
+}
