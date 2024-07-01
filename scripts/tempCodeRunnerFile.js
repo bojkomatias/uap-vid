eb6913fec79c36129124206f21ec521113cc9818
bojkomatias <bojko.matias@gmail.com>
2024-07-01 16:43:33 -0300

    Fixed build breaking, just some types and props changes

--- scripts/tempCodeRunnerFile.js
+++ scripts/tempCodeRunnerFile.js
@@ -1,20 +1,21 @@
- // console.log('Prepared protocols for update:', protocolsForMongo.length)
+// @ts-ignore
+// console.log('Prepared protocols for update:', protocolsForMongo.length)
 
-        // for (const p of protocolsForMongo) {
-        //     try {
-        //         const result = await protocolCollection.updateOne(
-        //             { _id: new ObjectId(p._id) },
-        //             {
-        //                 $set: {
-        //                     'sections.identification.academicUnitIds':
-        //                         p.sections.identification.academicUnitIds,
-        //                 },
-        //             }
-        //         )
-        //         console.log(
-        //             `Updated protocol ${p._id}: ${result.modifiedCount} document modified`
-        //         )
-        //     } catch (error) {
-        //         console.error(`Error updating protocol ${p._id}:`, error)
-        //     }
-        // }
+// for (const p of protocolsForMongo) {
+//     try {
+//         const result = await protocolCollection.updateOne(
+//             { _id: new ObjectId(p._id) },
+//             {
+//                 $set: {
+//                     'sections.identification.academicUnitIds':
+//                         p.sections.identification.academicUnitIds,
+//                 },
+//             }
+//         )
+//         console.log(
+//             `Updated protocol ${p._id}: ${result.modifiedCount} document modified`
+//         )
+//     } catch (error) {
+//         console.error(`Error updating protocol ${p._id}:`, error)
+//     }
+// }
