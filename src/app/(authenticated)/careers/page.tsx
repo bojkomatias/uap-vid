a45693483806b013a0f8c0b5d5bf17819888e876
nicohorn <nicoskate000@gmail.com>
2024-07-03 03:25:32 -0300

    Finished career CRUD
    Finished the basic CRUD, might need some improvements

--- src/app/(authenticated)/careers/page.tsx
+++ src/app/(authenticated)/careers/page.tsx
@@ -2,6 +2,7 @@ import { CareerTable } from 'modules/careers/careers-table'
 import { getAllCareers } from '@repositories/career'
 import { NewConvocatoryDialog } from '@convocatory/new-convocatory-dialog'
 import { Heading } from '@components/heading'
+import { NewCareerDialog } from 'modules/careers/new-career-dialog'
 
 export default async function Page({
   searchParams,
@@ -14,7 +15,7 @@ export default async function Page({
     <>
       <div className="flex items-end">
         <Heading>Carreras</Heading>
-        {/* <NewConvocatoryDialog /> */}
+        <NewCareerDialog />
       </div>
 
       <CareerTable totalRecords={totalRecords} careers={careers} />
