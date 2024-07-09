2d3de5e672f72a9fb1c1fc95346a0d2c05fc01e2
nicohorn <nicoskate000@gmail.com>
2024-07-09 20:54:08 -0300

    Added backend folder
    For now it only contains a server.js file which is the websocket server for the chat. It listens on the port 3001 of localhost, which should be replaced for an environment variable.

---
+++ backend/server.js
@@ -0,0 +1,60 @@
+// Import WebSocket and HTTP libraries
+const WebSocket = require('ws')
+const http = require('http')
+
+// Create an HTTP server and a WebSocket server on top of it
+const server = http.createServer()
+const wss = new WebSocket.Server({ server })
+
+// Initialize an array to store chat messages
+let chatMessages = []
+const MESSAGE_TYPE = {
+    INITIAL_DATA: 'INITIAL_DATA',
+    SEND_MESSAGE: 'SEND_MESSAGE',
+    NEW_MESSAGE: 'NEW_MESSAGE',
+}
+
+// Listen for new WebSocket connections
+wss.on('connection', (ws) => {
+    console.log('Client connected.')
+
+    // Send the initial chat messages to the newly connected client
+    ws.send(
+        JSON.stringify({
+            type: MESSAGE_TYPE.INITIAL_DATA,
+            payload: chatMessages,
+        })
+    )
+
+    // Listen for incoming messages from the client
+    ws.on('message', (message) => {
+        const parsedMessage = JSON.parse(message)
+        if (parsedMessage.type === MESSAGE_TYPE.SEND_MESSAGE) {
+            const { content } = JSON.parse(parsedMessage.content)
+            chatMessages.push(content)
+            console.log(chatMessages)
+            // Iterate through all connected clients and send the new message to them
+            wss.clients.forEach((client) => {
+                if (client.readyState === WebSocket.OPEN) {
+                    client.send(
+                        JSON.stringify({
+                            type: MESSAGE_TYPE.NEW_MESSAGE,
+                            payload: content,
+                        })
+                    )
+                }
+            })
+        }
+    })
+
+    // Listen for the 'close' event to log when a client disconnects
+    ws.on('close', () => {
+        console.log('Client disconnected.')
+    })
+})
+
+// Start the server and listen on a specific port
+const port = process.env.PORT || 3001
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
