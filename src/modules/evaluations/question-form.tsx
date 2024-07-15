58fd64c60f01a5a84050150dcc387a2adb73008e
nicohorn <nicoskate000@gmail.com>
2024-07-15 15:41:12 -0300

    Almost finished

--- src/modules/evaluations/question-form.tsx
+++ src/modules/evaluations/question-form.tsx
@@ -1,11 +1,15 @@
-import { Fieldset } from '@components/fieldset'
+import { FieldGroup, Fieldset, FormActions } from '@components/fieldset'
+import { notifications } from '@elements/notifications'
 import { useForm, zodResolver } from '@mantine/form'
 import type { ReviewQuestion } from '@prisma/client'
 import { updateQuestion } from '@repositories/review-question'
+import { FormButton } from '@shared/form/form-button'
+import { FormCombobox } from '@shared/form/form-combobox'
 import { FormInput } from '@shared/form/form-input'
 import { FormSwitch } from '@shared/form/form-switch'
 import { useMutation } from '@tanstack/react-query'
 import { ReviewQuestionSchema } from '@utils/zod'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 import type { z } from 'zod'
 
@@ -23,7 +27,9 @@ export default function QuestionForm({
     validate: zodResolver(ReviewQuestionSchema),
   })
 
-  const { data, isSuccess } = useMutation({
+  const router = useRouter()
+
+  const { mutate, isPending } = useMutation({
     mutationKey: [question],
     mutationFn: async ({
       data,
@@ -32,14 +38,26 @@ export default function QuestionForm({
       data: Omit<ReviewQuestion, 'id'>
       id: string
     }) => {
-      await updateQuestion(data, id)
+      const result = await updateQuestion(data, id)
+      if (result) {
+        notifications.show({
+          title: 'Pregunta de evaluación actualizada',
+          message: 'Se modificó la pregunta de evaluación con éxito',
+          intent: 'success',
+        })
+        router.refresh()
+      }
     },
   })
 
   return (
-    <form>
+    <form
+      onSubmit={form.onSubmit((values) => {
+        mutate({ id: question.id, data: values })
+      })}
+    >
       <Fieldset>
-        <div>
+        <FieldGroup>
           <FormSwitch
             label="Estado de la pregunta"
             description={
@@ -48,14 +66,28 @@ export default function QuestionForm({
             checked={form.getInputProps('active').value}
             {...form.getInputProps('active')}
           />
-        </div>
-        <FormInput
-          value={form.getInputProps('question').value}
-          description="Editar pregunta"
-          label="Texto de la pregunta"
-          {...form.getInputProps('question')}
-        />
+          <FormInput
+            value={form.getInputProps('question').value}
+            description="Editar pregunta"
+            label="Texto de la pregunta"
+            {...form.getInputProps('question')}
+          />
+          <FormCombobox
+            label="Tipo de evaluación"
+            value={form.getInputProps('type').value}
+            options={[
+              { value: 'SCIENTIFIC', label: 'Científica' },
+              { value: 'METHODOLOGIC', label: 'Metodológica' },
+            ]}
+            {...form.getInputProps('type')}
+          />
+        </FieldGroup>
       </Fieldset>
+      <FormActions>
+        <FormButton isLoading={isPending}>
+          Actualizar pregunta de evaluación
+        </FormButton>
+      </FormActions>
     </form>
   )
 }
