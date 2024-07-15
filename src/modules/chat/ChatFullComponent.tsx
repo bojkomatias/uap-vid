c5cd153e1d46724c1353fe5970b586dc3212eff6
nicohorn <nicoskate000@gmail.com>
2024-07-15 10:10:30 -0300

    Removed query client provider
    There's already a provider in an upper level

--- src/modules/chat/ChatFullComponent.tsx
+++ src/modules/chat/ChatFullComponent.tsx
@@ -1,6 +1,5 @@
 'use client'
 import React from 'react'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { WebSocketMessagesProvider } from './WebSocketProvider'
 import ChatComponent from './ChatForm'
 import type { User } from '@prisma/client'
@@ -11,13 +10,9 @@ export default function ChatFullComponent({
   user: User
   protocolId: string
 }) {
-  const queryClient = new QueryClient({})
-
   return (
-    <QueryClientProvider client={queryClient}>
-      <WebSocketMessagesProvider queryKey={['messages', protocolId]}>
-        <ChatComponent protocolId={protocolId} user={user} />
-      </WebSocketMessagesProvider>
-    </QueryClientProvider>
+    <WebSocketMessagesProvider queryKey={['messages', protocolId]}>
+      <ChatComponent protocolId={protocolId} user={user} />
+    </WebSocketMessagesProvider>
   )
 }
