fd46dea0b17175c7fc2cfa01b3ec99f17b28abc3
nicohorn <nicoskate000@gmail.com>
2024-07-16 20:41:34 -0300

    Fixed scope

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
     reactStrictMode: true,
-    output: "standalone"
+    output: 'standalone',
+    webpack: (config) => {
+        let modularizeImports = null
+        config.module.rules.some((rule) =>
+            rule.oneOf?.some((oneOf) => {
+                modularizeImports =
+                    oneOf?.use?.options?.nextConfig?.modularizeImports
+                return modularizeImports
+            })
+        )
+        if (modularizeImports?.['@headlessui/react'])
+            delete modularizeImports['@headlessui/react']
+        return config
+    },
 }
--- src/utils/scopes.ts
+++ src/utils/scopes.ts
@@ -9,26 +9,26 @@ import { ROLE, STATE, ACTION, ACCESS } from './zod'
  * - Check access to resource according to user role
  */
 const ROLE_ACCESS: { [key in keyof typeof ROLE]: AccessType[] } = {
-    [ROLE.RESEARCHER]: [ACCESS.PROTOCOLS, ACCESS.REVIEWS, ACCESS.USERS],
-    [ROLE.SECRETARY]: [
-        ACCESS.PROTOCOLS,
-        ACCESS.REVIEWS,
-        ACCESS.EVALUATORS,
-        ACCESS.USERS,
-    ],
-    [ROLE.METHODOLOGIST]: [ACCESS.PROTOCOLS, ACCESS.USERS],
-    [ROLE.SCIENTIST]: [ACCESS.PROTOCOLS, ACCESS.USERS],
-    [ROLE.ADMIN]: [
-        ACCESS.PROTOCOLS,
-        ACCESS.CONVOCATORIES,
-        ACCESS.REVIEWS,
-        ACCESS.EVALUATORS,
-        ACCESS.USERS,
-        ACCESS.ACADEMIC_UNITS,
-        ACCESS.TEAM_MEMBERS,
-        ACCESS.MEMBER_CATEGORIES,
-        ACCESS.ANUAL_BUDGETS,
-    ],
+  [ROLE.RESEARCHER]: [ACCESS.PROTOCOLS, ACCESS.REVIEWS, ACCESS.USERS],
+  [ROLE.SECRETARY]: [
+    ACCESS.PROTOCOLS,
+    ACCESS.REVIEWS,
+    ACCESS.EVALUATORS,
+    ACCESS.USERS,
+  ],
+  [ROLE.METHODOLOGIST]: [ACCESS.PROTOCOLS, ACCESS.USERS],
+  [ROLE.SCIENTIST]: [ACCESS.PROTOCOLS, ACCESS.USERS],
+  [ROLE.ADMIN]: [
+    ACCESS.PROTOCOLS,
+    ACCESS.CONVOCATORIES,
+    ACCESS.REVIEWS,
+    ACCESS.EVALUATORS,
+    ACCESS.USERS,
+    ACCESS.ACADEMIC_UNITS,
+    ACCESS.TEAM_MEMBERS,
+    ACCESS.MEMBER_CATEGORIES,
+    ACCESS.ANUAL_BUDGETS,
+  ],
 }
 
 /** Role Action Scope
@@ -36,86 +36,87 @@ const ROLE_ACCESS: { [key in keyof typeof ROLE]: AccessType[] } = {
  * - EDIT_BY_OWNER & PUBLISH can only be done for users that are protocol owners.
  */
 const ROLE_SCOPE: { [key in keyof typeof ROLE]: ActionType[] } = {
-    [ROLE.RESEARCHER]: [
-        ACTION.CREATE,
-        ACTION.EDIT_BY_OWNER,
-        ACTION.PUBLISH,
-        ACTION.VIEW_ANUAL_BUDGET,
-    ],
-    [ROLE.SECRETARY]: [
-        ACTION.ACCEPT,
-        ACTION.CREATE,
-        ACTION.EDIT,
-        ACTION.EDIT_BY_OWNER,
-        ACTION.PUBLISH,
-        ACTION.VIEW_ANUAL_BUDGET,
-    ],
-    [ROLE.METHODOLOGIST]: [
-        ACTION.REVIEW,
-        ACTION.CREATE,
-        ACTION.EDIT_BY_OWNER,
-        ACTION.PUBLISH,
-    ],
-    [ROLE.SCIENTIST]: [ACTION.REVIEW],
-    [ROLE.ADMIN]: [
-        ACTION.CREATE,
-        ACTION.EDIT,
-        ACTION.EDIT_BY_OWNER,
-        ACTION.PUBLISH,
-        ACTION.ACCEPT,
-        ACTION.REVIEW,
-        ACTION.APPROVE,
-        ACTION.ASSIGN_TO_METHODOLOGIST,
-        ACTION.ASSIGN_TO_SCIENTIFIC,
-        ACTION.DELETE,
-        ACTION.DISCONTINUE,
-        ACTION.FINISH,
-        ACTION.VIEW_ANUAL_BUDGET,
-        ACTION.GENERATE_ANUAL_BUDGET,
-    ],
+  [ROLE.RESEARCHER]: [
+    ACTION.CREATE,
+    ACTION.EDIT_BY_OWNER,
+    ACTION.PUBLISH,
+    ACTION.VIEW_ANUAL_BUDGET,
+  ],
+  [ROLE.SECRETARY]: [
+    ACTION.ACCEPT,
+    ACTION.CREATE,
+    ACTION.EDIT,
+    ACTION.EDIT_BY_OWNER,
+    ACTION.PUBLISH,
+    ACTION.VIEW_ANUAL_BUDGET,
+    ACTION.ASSIGN_TO_METHODOLOGIST,
+  ],
+  [ROLE.METHODOLOGIST]: [
+    ACTION.REVIEW,
+    ACTION.CREATE,
+    ACTION.EDIT_BY_OWNER,
+    ACTION.PUBLISH,
+  ],
+  [ROLE.SCIENTIST]: [ACTION.REVIEW],
+  [ROLE.ADMIN]: [
+    ACTION.CREATE,
+    ACTION.EDIT,
+    ACTION.EDIT_BY_OWNER,
+    ACTION.PUBLISH,
+    ACTION.ACCEPT,
+    ACTION.REVIEW,
+    ACTION.APPROVE,
+    ACTION.ASSIGN_TO_METHODOLOGIST,
+    ACTION.ASSIGN_TO_SCIENTIFIC,
+    ACTION.DELETE,
+    ACTION.DISCONTINUE,
+    ACTION.FINISH,
+    ACTION.VIEW_ANUAL_BUDGET,
+    ACTION.GENERATE_ANUAL_BUDGET,
+  ],
 }
 
 /** State Action Scope
  * - Check if an action can be performed according current protocol state
  */
 const STATE_SCOPE: { [key in keyof typeof STATE]: ActionType[] } = {
-    [STATE.NOT_CREATED]: [ACTION.CREATE],
-    [STATE.DRAFT]: [ACTION.EDIT_BY_OWNER, ACTION.PUBLISH, ACTION.DELETE],
-    [STATE.PUBLISHED]: [
-        ACTION.ASSIGN_TO_METHODOLOGIST,
-        ACTION.EDIT,
-        ACTION.DISCONTINUE,
-        ACTION.DELETE,
-    ],
-    [STATE.METHODOLOGICAL_EVALUATION]: [
-        ACTION.ASSIGN_TO_METHODOLOGIST, // It's a Re-assignation
-        ACTION.EDIT_BY_OWNER,
-        ACTION.REVIEW,
-        ACTION.ASSIGN_TO_SCIENTIFIC,
-        ACTION.DISCONTINUE,
-    ],
-    [STATE.SCIENTIFIC_EVALUATION]: [
-        ACTION.ASSIGN_TO_SCIENTIFIC, // Allows re-assignation
-        ACTION.EDIT_BY_OWNER,
-        ACTION.REVIEW,
-        ACTION.ACCEPT,
-        ACTION.DISCONTINUE,
-    ],
-    [STATE.ACCEPTED]: [
-        ACTION.APPROVE,
-        ACTION.DISCONTINUE,
-        ACTION.EDIT,
-        ACTION.GENERATE_ANUAL_BUDGET,
-        ACTION.VIEW_ANUAL_BUDGET,
-    ],
-    [STATE.ON_GOING]: [
-        ACTION.FINISH,
-        ACTION.VIEW_ANUAL_BUDGET,
-        ACTION.GENERATE_ANUAL_BUDGET,
-    ],
-    [STATE.FINISHED]: [],
-    [STATE.DISCONTINUED]: [],
-    [STATE.DELETED]: [],
+  [STATE.NOT_CREATED]: [ACTION.CREATE],
+  [STATE.DRAFT]: [ACTION.EDIT_BY_OWNER, ACTION.PUBLISH, ACTION.DELETE],
+  [STATE.PUBLISHED]: [
+    ACTION.ASSIGN_TO_METHODOLOGIST,
+    ACTION.EDIT,
+    ACTION.DISCONTINUE,
+    ACTION.DELETE,
+  ],
+  [STATE.METHODOLOGICAL_EVALUATION]: [
+    ACTION.ASSIGN_TO_METHODOLOGIST, // It's a Re-assignation
+    ACTION.EDIT_BY_OWNER,
+    ACTION.REVIEW,
+    ACTION.ASSIGN_TO_SCIENTIFIC,
+    ACTION.DISCONTINUE,
+  ],
+  [STATE.SCIENTIFIC_EVALUATION]: [
+    ACTION.ASSIGN_TO_SCIENTIFIC, // Allows re-assignation
+    ACTION.EDIT_BY_OWNER,
+    ACTION.REVIEW,
+    ACTION.ACCEPT,
+    ACTION.DISCONTINUE,
+  ],
+  [STATE.ACCEPTED]: [
+    ACTION.APPROVE,
+    ACTION.DISCONTINUE,
+    ACTION.EDIT,
+    ACTION.GENERATE_ANUAL_BUDGET,
+    ACTION.VIEW_ANUAL_BUDGET,
+  ],
+  [STATE.ON_GOING]: [
+    ACTION.FINISH,
+    ACTION.VIEW_ANUAL_BUDGET,
+    ACTION.GENERATE_ANUAL_BUDGET,
+  ],
+  [STATE.FINISHED]: [],
+  [STATE.DISCONTINUED]: [],
+  [STATE.DELETED]: [],
 }
 
 /**
@@ -125,7 +126,7 @@ const STATE_SCOPE: { [key in keyof typeof STATE]: ActionType[] } = {
  * @returns
  */
 export const canAccess = (access: AccessType, role: RoleType) =>
-    ROLE_ACCESS[role].includes(access)
+  ROLE_ACCESS[role].includes(access)
 
 /**
  * Check Execution Permission according to State and Role
@@ -135,7 +136,7 @@ export const canAccess = (access: AccessType, role: RoleType) =>
  * @returns
  */
 export const canExecute = (
-    action: ActionType,
-    role: RoleType,
-    state: StateType
+  action: ActionType,
+  role: RoleType,
+  state: StateType
 ) => ROLE_SCOPE[role].includes(action) && STATE_SCOPE[state].includes(action)
