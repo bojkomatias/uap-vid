453d56b71a97402adfbde4f1daffc90872c1df69
nicohorn <nicoskate000@gmail.com>
2024-07-24 18:54:55 -0300

    Removed code from testing endpoint

--- src/app/api/testing/route.ts
+++ src/app/api/testing/route.ts
@@ -1,14 +1,5 @@
-import { emailer, getEmailContent, getEmailSubjects } from '@utils/emailer'
-import { useCases } from '@utils/emailer/use-cases'
 import { NextResponse } from 'next/server'
 
 export async function GET() {
-  const email = await emailer({
-    useCase: useCases.changeUserEmail,
-    email: 'contact@nicohorn.com',
-    randomString: '11111',
-  })
-
-  console.log(email)
   return NextResponse.json({ hello: 'world' })
 }
