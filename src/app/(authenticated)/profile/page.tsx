e1725e2ab988b16b36dd31bd96d8e2bd4f45b78c
bojkomatias <bojko.matias@gmail.com>
2024-07-24 18:54:53 -0300

    IUnitial revamping of profile

--- src/app/(authenticated)/profile/page.tsx
+++ src/app/(authenticated)/profile/page.tsx
@@ -4,26 +4,82 @@ import { ReviewerCertificatePDF } from 'modules/profile/reviewer-certificate'
 import { getReviewsByReviewerId } from '@repositories/review'
 import { ProfileInfo } from 'modules/profile/profile-info'
 import { Role } from '@prisma/client'
-import { Heading } from '@components/heading'
+import { Heading, Subheading } from '@components/heading'
+import RolesDictionary from '@utils/dictionaries/RolesDictionary'
+import Image from 'next/image'
+import { UserCircle } from 'tabler-icons-react'
+import {
+  DescriptionDetails,
+  DescriptionList,
+  DescriptionTerm,
+} from '@components/description-list'
+import { Button } from '@components/button'
+import { Text } from '@components/text'
 
 export default async function Page() {
   const session = await getServerSession(authOptions)
   if (!session) return
+  const user = session.user
   const reviews = await getReviewsByReviewerId(session.user.id)
 
   return (
     <>
-      <Heading>Perfil de usuario</Heading>
-      <ProfileInfo
-        certificate={
-          // Since not all users do evaluations/reviews, I'm checking for the user role before loading the component, therefore, improving the load time of the page.
-          (session.user.role == Role.SCIENTIST ||
-            session.user.role == Role.METHODOLOGIST) && (
-            <ReviewerCertificatePDF user={session.user} reviews={reviews} />
-          )
-        }
-        user={session.user}
-      />
+      <Heading>Cuenta</Heading>
+      <Subheading className="mb-4">
+        Datos del usuario con el cual se encuentra autenticado en el sistema.
+      </Subheading>
+      {user.image ?
+        <Image
+          className="size-12 rounded-full"
+          src={user.image}
+          alt="Imagen del usuario"
+          height={200}
+          width={200}
+        />
+      : null}
+      <DescriptionList className="mt-4">
+        <DescriptionTerm>UUID</DescriptionTerm>
+        <DescriptionDetails>
+          <span className="font-mono text-xs">{user.id}</span>
+        </DescriptionDetails>
+        <DescriptionTerm>Nombre</DescriptionTerm>
+        <DescriptionDetails>{user.name}</DescriptionDetails>
+        <DescriptionTerm>DNI</DescriptionTerm>
+        <DescriptionDetails>{user.dni}</DescriptionDetails>
+        <DescriptionTerm>Email</DescriptionTerm>
+        <DescriptionDetails>{user.email}</DescriptionDetails>
+        <DescriptionTerm>Rol</DescriptionTerm>
+        <DescriptionDetails>{RolesDictionary[user.role]}</DescriptionDetails>
+      </DescriptionList>
+
+      <Heading className="mt-8">Editar cuenta</Heading>
+      {!user.password ?
+        <div className="flex items-end justify-between">
+          <Text className="max-w-3xl">
+            La cuenta esta relacionada directamente al a cuenta de Microsoft
+            Office. Si desea editar algún dato, debe hacerlo desde Microsoft y
+            se verá reflejado aquí la proxima vez que ingrese al sistema.
+          </Text>
+          <Button
+            href={'https://www.office.com/'}
+            color="light"
+            className="h-10 shrink-0 !gap-0 whitespace-nowrap"
+          >
+            Editar cuenta en
+            <Image
+              src={'/whitebackgroundmicrosoft.png'}
+              alt="Microsoft Logo"
+              width={90}
+              height={50}
+            />
+          </Button>
+        </div>
+      : <ProfileInfo user={user} />}
+
+      {/*  Since not all users do evaluations/reviews, I'm checking for the user role before loading the component, therefore, improving the load time of the page. */}
+      {(user.role == Role.SCIENTIST || user.role == Role.METHODOLOGIST) && (
+        <ReviewerCertificatePDF user={session.user} reviews={reviews} />
+      )}
     </>
   )
 }
--- src/modules/profile/profile-info.tsx
+++ src/modules/profile/profile-info.tsx
@@ -17,13 +17,7 @@ import Link from 'next/link'
 import React, { useState } from 'react'
 import { Mail, Password, UserCircle } from 'tabler-icons-react'
 
-export function ProfileInfo({
-  certificate,
-  user,
-}: {
-  certificate?: React.ReactNode
-  user: User
-}) {
+export function ProfileInfo({ user }: { user: User }) {
   const emailForm = useForm({
     initialValues: {
       currentEmail: user.email,
@@ -116,236 +110,192 @@ export function ProfileInfo({
   }
 
   return (
-    <div className="flex flex-col justify-between gap-4 rounded-md border-gray-200 text-gray-600">
-      <div>
-        <div key={user?.name} className=" flex flex-col gap-10   sm:flex-row">
-          <div className="flex flex-grow items-center gap-x-6">
-            <h3 className=" w-full border-b text-base font-semibold leading-7 tracking-tight text-gray-900">
-              {user.name}
-            </h3>
-            {user.image ?
-              <Image
-                className="h-12 w-12 rounded-full"
-                src={user.image as string}
-                alt=""
-                height={200}
-                width={200}
-              />
-            : <UserCircle width={48} height={48} />}
+    <>
+      <DisclosureComponent
+        title={
+          <div className="flex items-center gap-2 font-semibold">
+            <Mail height={20} /> Cambiar Email
           </div>
-        </div>
-        <p className="text-sm font-semibold leading-6 text-indigo-600">
-          {RolesDictionary[user.role]}
-        </p>
-        <p className="text-sm font-semibold leading-6 text-indigo-600">
-          {user.email}
-        </p>
-      </div>
-      <h3 className="border-b">Editar cuenta</h3>{' '}
-      {!user.password ?
-        <Link
-          className={cx(buttonStyle('outline'))}
-          href={'https://www.office.com/'}
-        >
-          Editar cuenta en{' '}
-          <Image
-            src={'/whitebackgroundmicrosoft.png'}
-            alt="Microsoft Logo"
-            width={100}
-            height={100}
-          />
-        </Link>
-      : <>
-          <DisclosureComponent
-            title={
-              <div className="flex items-center gap-2 font-semibold">
-                <Mail height={20} /> Cambiar Email
-              </div>
-            }
-          >
-            <form>
-              <div>
-                <label htmlFor="newEmail" className="label">
-                  Nuevo email
-                </label>
-                <div className="flex w-full gap-2">
-                  <div className="flex-grow">
-                    <input
-                      className="input h-8 text-sm placeholder:lowercase"
-                      placeholder="ejemplo@uap.edu.ar"
-                      id="newEmail"
-                      {...emailForm.getInputProps('newEmail')}
-                    />
+        }
+      >
+        <form>
+          <div>
+            <label htmlFor="newEmail" className="label">
+              Nuevo email
+            </label>
+            <div className="flex w-full gap-2">
+              <div className="flex-grow">
+                <input
+                  className="input h-8 text-sm placeholder:lowercase"
+                  placeholder="ejemplo@uap.edu.ar"
+                  id="newEmail"
+                  {...emailForm.getInputProps('newEmail')}
+                />
 
-                    {emailForm.getInputProps('newEmail').error ?
-                      <p className="error">
-                        *{emailForm.getInputProps('newEmail').error}
-                      </p>
-                    : null}
-                  </div>
-                  <Button
-                    className="h-8 w-fit shadow-sm"
-                    intent="secondary"
-                    onClick={async () => {
-                      if (!emailForm.getInputProps('newEmail').error) {
-                        await sendEmail({
-                          email: user.email,
-                          randomString: random!,
-                        })
-
-                        document
-                          .getElementById('emailCodeConfirmation')
-                          ?.classList.remove('hidden')
-                        document
-                          .getElementById('emailCodeConfirmation')
-                          ?.classList.add('fade-in')
-                      }
-                    }}
-                  >
-                    Confirmar
-                  </Button>
-                </div>
-              </div>
-              <div id="emailCodeConfirmation" className="hidden">
-                <div className="my-2">
-                  <label htmlFor="emailCode" className="label">
-                    Código
-                  </label>
-                  <div className="flex w-full flex-col gap-2">
-                    <div className="flex-grow">
-                      <input
-                        className="input h-8 text-sm placeholder:lowercase"
-                        placeholder="Ejemplo: 1qyzy"
-                        id="emailCode"
-                        {...emailForm.getInputProps('emailCode')}
-                      />
-
-                      {emailForm.getInputProps('emailCode').error ?
-                        <p className="error">
-                          *{emailForm.getInputProps('emailCode').error}
-                        </p>
-                      : null}
-                    </div>
-                  </div>
-                </div>
-                <Button
-                  className=" ml-auto mt-3 h-8 w-fit shadow-sm"
-                  intent="primary"
-                  onClick={async () => {
-                    if (!emailForm.getInputProps('emailCode').error) {
-                      await updateUserEmail({
-                        id: user.id,
-                        email: emailForm.getInputProps('newEmail').value,
-                      })
-                    }
-                  }}
-                >
-                  Cambiar email
-                </Button>
-              </div>
-            </form>
-          </DisclosureComponent>
-          <DisclosureComponent
-            title={
-              <div className="flex items-center gap-2 font-semibold">
-                <Password width={20} />
-                Cambiar Contraseña
-              </div>
-            }
-          >
-            <form className="flex flex-col gap-6">
-              {' '}
-              <div>
-                {' '}
-                <label htmlFor="currentPassword" className="label">
-                  Contraseña actual
-                </label>
-                <div className="flex w-full gap-2">
-                  <div className="flex-grow">
-                    <input
-                      type="password"
-                      className="input h-8 text-sm placeholder:lowercase"
-                      placeholder="••••••••••••••"
-                      id="currentPassword"
-                      {...passwordForm.getInputProps('currentPassword')}
-                    />
-
-                    {passwordForm.getInputProps('currentPassword').error ?
-                      <p className="error">
-                        *{passwordForm.getInputProps('currentPassword').error}
-                      </p>
-                    : null}
-                  </div>
-                </div>
-              </div>
-              <div>
-                {' '}
-                <label htmlFor="newPassword" className="label">
-                  Nueva contraseña
-                </label>
-                <div className="flex w-full gap-2">
-                  <div className="flex-grow">
-                    <input
-                      type="password"
-                      className="input h-8 text-sm placeholder:lowercase"
-                      placeholder="••••••••••••••"
-                      id="newPassword"
-                      {...passwordForm.getInputProps('newPassword')}
-                    />
-
-                    {passwordForm.getInputProps('newPassword').error ?
-                      <p className="error">
-                        *{passwordForm.getInputProps('newPassword').error}
-                      </p>
-                    : null}
-                  </div>
-                </div>
-              </div>
-              <div>
-                {' '}
-                <label htmlFor="newPasswordConfirm" className="label">
-                  Confirmar nueva contraseña
-                </label>
-                <div className="flex w-full gap-2">
-                  <div className="flex-grow">
-                    <input
-                      type="password"
-                      className="input h-8 text-sm placeholder:lowercase"
-                      placeholder="••••••••••••••"
-                      id="newPasswordConfirm"
-                      {...passwordForm.getInputProps('newPasswordConfirm')}
-                    />
-
-                    {passwordForm.getInputProps('newPasswordConfirm').error ?
-                      <p className="error">
-                        *
-                        {passwordForm.getInputProps('newPasswordConfirm').error}
-                      </p>
-                    : null}
-                  </div>
-                </div>
+                {emailForm.getInputProps('newEmail').error ?
+                  <p className="error">
+                    *{emailForm.getInputProps('newEmail').error}
+                  </p>
+                : null}
               </div>
               <Button
-                className=" ml-auto h-8 w-fit shadow-sm"
-                intent="primary"
+                className="h-8 w-fit shadow-sm"
+                intent="secondary"
                 onClick={async () => {
-                  await updateUserPassword({
-                    id: user.id,
-                    currentPasswordHash: user.password!,
-                    currentPassword:
-                      passwordForm.getInputProps('currentPassword').value,
-                    newPassword:
-                      passwordForm.getInputProps('newPassword').value,
-                  })
+                  if (!emailForm.getInputProps('newEmail').error) {
+                    await sendEmail({
+                      email: user.email,
+                      randomString: random!,
+                    })
+
+                    document
+                      .getElementById('emailCodeConfirmation')
+                      ?.classList.remove('hidden')
+                    document
+                      .getElementById('emailCodeConfirmation')
+                      ?.classList.add('fade-in')
+                  }
                 }}
               >
-                Cambiar contraseña
+                Confirmar
               </Button>
-            </form>
-          </DisclosureComponent>
-        </>
-      }
-      {certificate}
-    </div>
+            </div>
+          </div>
+          <div id="emailCodeConfirmation" className="hidden">
+            <div className="my-2">
+              <label htmlFor="emailCode" className="label">
+                Código
+              </label>
+              <div className="flex w-full flex-col gap-2">
+                <div className="flex-grow">
+                  <input
+                    className="input h-8 text-sm placeholder:lowercase"
+                    placeholder="Ejemplo: 1qyzy"
+                    id="emailCode"
+                    {...emailForm.getInputProps('emailCode')}
+                  />
+
+                  {emailForm.getInputProps('emailCode').error ?
+                    <p className="error">
+                      *{emailForm.getInputProps('emailCode').error}
+                    </p>
+                  : null}
+                </div>
+              </div>
+            </div>
+            <Button
+              className=" ml-auto mt-3 h-8 w-fit shadow-sm"
+              intent="primary"
+              onClick={async () => {
+                if (!emailForm.getInputProps('emailCode').error) {
+                  await updateUserEmail({
+                    id: user.id,
+                    email: emailForm.getInputProps('newEmail').value,
+                  })
+                }
+              }}
+            >
+              Cambiar email
+            </Button>
+          </div>
+        </form>
+      </DisclosureComponent>
+      <DisclosureComponent
+        title={
+          <div className="flex items-center gap-2 font-semibold">
+            <Password width={20} />
+            Cambiar Contraseña
+          </div>
+        }
+      >
+        <form className="flex flex-col gap-6">
+          {' '}
+          <div>
+            {' '}
+            <label htmlFor="currentPassword" className="label">
+              Contraseña actual
+            </label>
+            <div className="flex w-full gap-2">
+              <div className="flex-grow">
+                <input
+                  type="password"
+                  className="input h-8 text-sm placeholder:lowercase"
+                  placeholder="••••••••••••••"
+                  id="currentPassword"
+                  {...passwordForm.getInputProps('currentPassword')}
+                />
+
+                {passwordForm.getInputProps('currentPassword').error ?
+                  <p className="error">
+                    *{passwordForm.getInputProps('currentPassword').error}
+                  </p>
+                : null}
+              </div>
+            </div>
+          </div>
+          <div>
+            {' '}
+            <label htmlFor="newPassword" className="label">
+              Nueva contraseña
+            </label>
+            <div className="flex w-full gap-2">
+              <div className="flex-grow">
+                <input
+                  type="password"
+                  className="input h-8 text-sm placeholder:lowercase"
+                  placeholder="••••••••••••••"
+                  id="newPassword"
+                  {...passwordForm.getInputProps('newPassword')}
+                />
+
+                {passwordForm.getInputProps('newPassword').error ?
+                  <p className="error">
+                    *{passwordForm.getInputProps('newPassword').error}
+                  </p>
+                : null}
+              </div>
+            </div>
+          </div>
+          <div>
+            {' '}
+            <label htmlFor="newPasswordConfirm" className="label">
+              Confirmar nueva contraseña
+            </label>
+            <div className="flex w-full gap-2">
+              <div className="flex-grow">
+                <input
+                  type="password"
+                  className="input h-8 text-sm placeholder:lowercase"
+                  placeholder="••••••••••••••"
+                  id="newPasswordConfirm"
+                  {...passwordForm.getInputProps('newPasswordConfirm')}
+                />
+
+                {passwordForm.getInputProps('newPasswordConfirm').error ?
+                  <p className="error">
+                    *{passwordForm.getInputProps('newPasswordConfirm').error}
+                  </p>
+                : null}
+              </div>
+            </div>
+          </div>
+          <Button
+            className=" ml-auto h-8 w-fit shadow-sm"
+            intent="primary"
+            onClick={async () => {
+              await updateUserPassword({
+                id: user.id,
+                currentPasswordHash: user.password!,
+                currentPassword:
+                  passwordForm.getInputProps('currentPassword').value,
+                newPassword: passwordForm.getInputProps('newPassword').value,
+              })
+            }}
+          >
+            Cambiar contraseña
+          </Button>
+        </form>
+      </DisclosureComponent>
+    </>
   )
 }
