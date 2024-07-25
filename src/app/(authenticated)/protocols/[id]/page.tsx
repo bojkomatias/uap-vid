7bff210ddea4fcc13b521c57f072fc3d43d93e3b
nicohorn <nicoskate000@gmail.com>
2024-07-25 16:21:39 -0300

    Now we have a PDF como la gente

--- src/app/(authenticated)/protocols/[id]/page.tsx
+++ src/app/(authenticated)/protocols/[id]/page.tsx
@@ -7,20 +7,20 @@ import { getServerSession } from 'next-auth'
 import { authOptions } from 'app/api/auth/[...nextauth]/auth'
 
 export default async function Page({ params }: { params: { id: string } }) {
-  if (params.id === 'new') redirect('/protocols/new/0')
-  const protocol = await findProtocolById(params.id)
+    if (params.id === 'new') redirect('/protocols/new/0')
+    const protocol = await findProtocolById(params.id)
 
-  if (!protocol) {
-    redirect('/protocols')
-  }
+    if (!protocol) {
+        redirect('/protocols')
+    }
 
-  const session = await getServerSession(authOptions)
+    const session = await getServerSession(authOptions)
 
-  return (
-    <>
-      {/* <PDF protocol={protocol} /> */}
-      <ChatFullComponent user={session!.user} protocolId={protocol.id} />
-      <View sections={protocol.sections} />
-    </>
-  )
+    return (
+        <>
+            <PDF />
+            <ChatFullComponent user={session!.user} protocolId={protocol.id} />
+            <View sections={protocol.sections} />
+        </>
+    )
 }
