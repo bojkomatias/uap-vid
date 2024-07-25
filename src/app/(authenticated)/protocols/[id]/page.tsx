e14cfb08323ce78addf0a2520e9b484c691a3106
nicohorn <nicoskate000@gmail.com>
2024-07-25 03:24:22 -0300

    Removed prop

--- src/app/(authenticated)/protocols/[id]/page.tsx
+++ src/app/(authenticated)/protocols/[id]/page.tsx
@@ -12,7 +12,7 @@ export default async function Page({ params }: { params: { id: string } }) {
 
     return (
         <>
-            <PDF protocol={protocol} />
+            <PDF />
             <View sections={protocol.sections} />
         </>
     )
