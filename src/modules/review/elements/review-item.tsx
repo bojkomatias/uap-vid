55baabd55219b8111b7ac43e3d1eaf22f41a016a
bojkomatias <bojko.matias@gmail.com>
2024-07-26 17:09:02 -0300

    Did magic

--- src/modules/review/elements/review-item.tsx
+++ src/modules/review/elements/review-item.tsx
@@ -15,6 +15,8 @@ import { useCases } from '@utils/emailer/use-cases'
 import { useQuery } from '@tanstack/react-query'
 import { getAllQuestions } from '@repositories/review-question'
 import { Subheading } from '@components/heading'
+import { BadgeButton } from '@components/badge'
+import { Text } from '@components/text'
 
 export default function ReviewItem({
   review,
@@ -42,74 +44,60 @@ export default function ReviewItem({
     queryKey: ['questions', review],
     queryFn: async () => await getAllQuestions(),
   })
-  console.log(data)
+
   if (review.verdict === ReviewVerdict.NOT_REVIEWED) return null
+
   return (
-    <li>
-      <Subheading>{ReviewTypesDictionary[review.type]}</Subheading>
-      <div
-        className={cx(
-          'rounded border',
-          review.revised ? 'bg-gray-50 opacity-70' : 'bg-white opacity-100'
-        )}
-      >
-        <button
-          className="group -mb-px flex w-full items-center justify-between space-x-4 rounded-t bg-gray-100 px-2 py-1 text-gray-500"
-          onClick={() => setShowReviewQuestions((prv) => !prv)}
-        >
-          <ReviewVerdictBadge verdict={review.verdict} />
-
-          {review.verdict === ReviewVerdict.APPROVED_WITH_CHANGES ?
-            isOwner ?
-              <ReviseCheckbox id={review.id} revised={review.revised} />
-            : <label className="label pointer-events-auto">
-                {review.revised ? 'revisado' : 'no revisado'}
-              </label>
-
-          : null}
-          <span className="flex text-xs font-semibold text-gray-600 group-hover:underline">
-            Ver detalles
-            <ChevronRight
-              className={cx(
-                'h-4 w-4 transition',
-                showReviewQuestions && 'rotate-90'
-              )}
-            />
-          </span>
-        </button>
-
-        {showReviewQuestions && data && (
-          <div className="space-y-4 py-4 pl-2 pr-4">
-            {isLoading ?
-              <Loader />
-            : <div>
-                {review.questions.map((question, index) => (
-                  <ReviewQuestionView
-                    key={question.id}
-                    index={index}
-                    questions={data}
-                    {...question}
-                  />
-                ))}
-              </div>
-            }
-          </div>
-        )}
-
-        <div className="-mt-px flex justify-end gap-1 px-3 py-0.5 text-xs">
-          <span className="font-semibold text-gray-700">
-            {role === Role.ADMIN || role === Role.SECRETARY ?
-              review.reviewer.name
-            : null}
-          </span>
-          <span className="font-light text-gray-500">
-            {new Date().getTime() - new Date(review.updatedAt).getTime() > 1 ?
-              getDuration(
-                new Date().getTime() - new Date(review.updatedAt).getTime()
-              )
-            : 'hace un instante'}
-          </span>
+    <li className="pr-px">
+      <Subheading className="flex justify-between">
+        {ReviewTypesDictionary[review.type]}
+        <ReviewVerdictBadge verdict={review.verdict} />
+      </Subheading>
+      <div className="mt-1 flex justify-between">
+        {review.verdict === ReviewVerdict.APPROVED_WITH_CHANGES ?
+          isOwner ?
+            <ReviseCheckbox id={review.id} revised={review.revised} />
+          : <Text>{review.revised ? 'Revisado' : 'No revisado'}</Text>
+        : null}
+        <BadgeButton onClick={() => setShowReviewQuestions((prv) => !prv)}>
+          Ver evaluación
+          <ChevronRight
+            data-slot="icon"
+            className={cx('size-4', showReviewQuestions ? 'rotate-90' : '')}
+          />
+        </BadgeButton>
+      </div>
+      {showReviewQuestions && data && (
+        <div className="space-y-2">
+          {isLoading ?
+            <Loader />
+          : review.questions.map((question, index) => (
+              <ReviewQuestionView
+                key={question.id}
+                index={index}
+                questions={data}
+                approved={question.approved}
+                comment={question.comment}
+                id={question.id}
+              />
+            ))
+          }
         </div>
+      )}
+
+      <div className="-mt-px flex justify-end gap-1 px-3 py-0.5 text-xs">
+        <span className="font-semibold text-gray-700">
+          {role === Role.ADMIN || role === Role.SECRETARY ?
+            review.reviewer.name
+          : null}
+        </span>
+        <span className="font-light text-gray-500">
+          {new Date().getTime() - new Date(review.updatedAt).getTime() > 1 ?
+            getDuration(
+              new Date().getTime() - new Date(review.updatedAt).getTime()
+            )
+          : 'hace un instante'}
+        </span>
       </div>
     </li>
   )
