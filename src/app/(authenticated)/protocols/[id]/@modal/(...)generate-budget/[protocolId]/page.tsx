f752b0bbbaee41afc58f06dd7e5f7766ce3a0e38
bojkomatias <bojko.matias@gmail.com>
2024-08-29 23:05:19 +0200

    Dialog and error parsing handled

--- src/app/(authenticated)/protocols/[id]/@modal/(...)generate-budget/[protocolId]/page.tsx
+++ src/app/(authenticated)/protocols/[id]/@modal/(...)generate-budget/[protocolId]/page.tsx
@@ -1,6 +1,9 @@
+import { notifications } from '@elements/notifications'
 import { AnualBudgetPreview } from '@protocol/budgets/anual-budget-preview'
+import { ErrorParsingTeamMembers } from '@protocol/budgets/error-parsing-team-members'
 import { GenerateAnualBudgetDialog } from '@protocol/budgets/generate-anual-budget-dialog'
 import { findProtocolByIdWithResearcher } from '@repositories/protocol'
+import { TeamMemberRelation } from '@utils/zod'
 
 export default async function ModalPage({
   params: { protocolId },
@@ -10,9 +13,15 @@ export default async function ModalPage({
   const protocol = await findProtocolByIdWithResearcher(protocolId)
   if (!protocol) return
 
+  const parsedObject = TeamMemberRelation.safeParse(
+    protocol.sections.identification.team
+  )
+
+  if (!parsedObject.success) return <ErrorParsingTeamMembers />
+
   return (
     <GenerateAnualBudgetDialog>
-      <AnualBudgetPreview protocol={protocol} />
+      <AnualBudgetPreview protocol={protocol} parsedData={parsedObject.data!} />
     </GenerateAnualBudgetDialog>
   )
 }
