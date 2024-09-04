0d450bef865e7c738e1fa5043e709c220cf0ae15
nicohorn <nicoskate000@gmail.com>
2024-09-04 17:14:19 -0300

    .

--- scripts/logs-transform.js
+++ scripts/logs-transform.js
@@ -1,23 +1,5 @@
 import { MongoClient, ObjectId } from 'mongodb'
 import 'dotenv/config'
-
-// const Actions {
-//     CREATE
-//     EDIT
-//     EDIT_BY_OWNER
-//     PUBLISH
-//     ASSIGN_TO_METHODOLOGIST
-//     ASSIGN_TO_SCIENTIFIC
-//     REVIEW
-//     ACCEPT //This action is made by the secretary. Accept the protocol to be evalualuated by the VID committee
-//     APPROVE //This approval is made by the admin and approve the protocol and mark it as ON_GOING
-//     DISCONTINUE
-//     FINISH
-//     DELETE
-//     GENERATE_ANUAL_BUDGET
-//     VIEW_ANUAL_BUDGET
-//   }
-
 const uri = process.env.MONGO_URI
 const client = new MongoClient(uri)
 
